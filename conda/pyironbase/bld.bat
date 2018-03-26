2293cc3b15e04bf40a3e3db155ea31a147534921
Jan Janßen <jan-janssen@users.noreply.github.com>
2018-03-26 16:00:33 +0200

    Update bld.bat

--- conda/pyironbase/bld.bat
+++ conda/pyironbase/bld.bat
@@ -1,8 +1,1 @@
-"%PYTHON%" setup.py install --offline
-if errorlevel 1 exit 1
-
-:: Add more build steps here, if they are necessary.
-
-:: See
-:: http://docs.continuum.io/conda/build.html
-:: for a list of environment variables that are set during the build process.
+pip install .
