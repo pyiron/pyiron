11818a40000091e6db997eb36b564b408cadb866
Jan Janßen <jan-janssen@users.noreply.github.com>
2018-03-26 11:25:57 +0200

    Update conf.py

--- conf.py
+++ conf.py
@@ -105,7 +105,7 @@ modindex_common_prefix = ['pyiron.']
 # a list of builtin themes.
 try:
     import sphinx_rtd_theme
-    import msmb_theme
+    # import msmb_theme
     html_theme = 'sphinx_rtd_theme'
     # html_theme = 'msmb'
 except ImportError:
