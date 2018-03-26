b54614f99b4570ae87b7fb038065a6fa9153941c
Jan Janßen <jan-janssen@users.noreply.github.com>
2018-03-26 11:27:03 +0200

    Update conf.py

--- conf.py
+++ conf.py
@@ -104,12 +104,14 @@ modindex_common_prefix = ['pyiron.']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 try:
-    import sphinx_rtd_theme
-    # import msmb_theme
-    html_theme = 'sphinx_rtd_theme'
-    # html_theme = 'msmb'
+    import msmb_theme    
+    html_theme = 'msmb'
 except ImportError:
-    html_theme = 'default'
+    try:
+        import sphinx_rtd_theme
+        html_theme = 'sphinx_rtd_theme'
+    except ImportError:
+        html_theme = 'default'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
