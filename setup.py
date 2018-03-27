6cd37dae94848d96f21aa0155ffac11f6f2809fe
Jan Janßen <jan-janssen@users.noreply.github.com>
2018-03-27 17:16:46 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyiron',
-    version='0.0.2',
+    version='0.0.3',
     description='pyiron IDE base',
     long_description='http://pyiron.org',
 
