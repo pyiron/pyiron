47c574f410a45198b1be1c28611063f42be7c6e7
Jan Janßen <jan-janssen@users.noreply.github.com>
2018-03-29 21:07:42 +0200

    Update generic.py

--- pyiron_base/core/settings/generic.py
+++ pyiron_base/core/settings/generic.py
@@ -219,7 +219,7 @@ class Settings(with_metaclass(Singleton)):
         pyiron_env = self._config.pyiron_envs
         if pyiron_env['type'] == 'SQLite':
             # SQLite is raising ugly error messages when the database directory does not exist.
-            os.makedirs(os.path.dirname(pyiron_env['file']), exist_ok=True)
+            os.makedirs(os.path.dirname(pyiron_env['file']))
             sql_con_str = 'sqlite:///' + pyiron_env['file']
             sql_db_table = pyiron_env['table_name']
         elif pyiron_env['type'] == 'Postgres':
