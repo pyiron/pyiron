4cd3a0f854b112ac55a84071349cc300383c9075
Jan Janßen <janssen@mpie.de>
2018-04-01 23:03:01 +0200

    Update tests for generic Settings

---
+++ tests/core/settings/test_generic_file_config.py
@@ -0,0 +1,40 @@
+from pyiron_base.core.settings.generic import Settings
+from pathlib2 import Path
+import unittest
+
+
+class TestConfigSettingsStatic(unittest.TestCase):
+    def setUp(self):
+        self.user_path = Path('~').expanduser().resolve().absolute().as_posix()
+        self.resource_path = Path('~/pyiron/resources').expanduser().resolve().absolute().as_posix()
+        self.project_path = Path('~/pyiron/projects').expanduser().resolve().absolute().as_posix() + '/'
+        self.file_config = Settings()
+
+    def test_file_db_connection_name(self):
+        self.assertEqual(self.file_config.db_connection_name, 'DEFAULT')
+
+    def test_file_db_connection_string(self):
+        self.assertEqual(self.file_config.db_connection_string, 'sqlite://' + self.resource_path + '/sqlite.db')
+
+    def test_file_db_connection_table(self):
+        self.assertEqual(self.file_config.db_connection_table, 'jobs_pyiron')
+
+    def test_file_db_translate_dict(self):
+        self.assertEqual(self.file_config.db_translate_dict,
+                         {'DEFAULT': {self.project_path: self.project_path}})
+
+    def test_file_db_name(self):
+        self.assertEqual(self.file_config.db_name, 'DEFAULT')
+
+    def test_file_top_path(self):
+        self.assertEqual(self.file_config.top_path(self.project_path + '/test'), self.project_path)
+
+    def test_file_resource_paths(self):
+        self.assertEqual(self.file_config.resource_paths, [self.resource_path])
+
+    def test_file_login_user(self):
+        self.assertEqual(self.file_config.login_user, 'pyiron')
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ tests/core/settings/test_generic_test_config.py
@@ -0,0 +1,40 @@
+import os
+from pyiron_base.core.settings.generic import Settings
+import unittest
+
+
+class TestConfigSettingsStatic(unittest.TestCase):
+    def setUp(self):
+        self.test_config = Settings(config={'file': 'genericsettings.db',
+                                            'top_level_dirs': os.path.abspath(os.getcwd()),
+                                            'resource_paths': os.path.abspath(os.getcwd())})
+
+    def test_db_connection_name(self):
+        self.assertEqual(self.test_config.db_connection_name, 'DEFAULT')
+
+    def test_db_connection_string(self):
+        self.assertEqual(self.test_config.db_connection_string, 'sqlite:///genericsettings.db')
+
+    def test_db_connection_table(self):
+        self.assertEqual(self.test_config.db_connection_table, 'jobs_pyiron')
+
+    def test_db_translate_dict(self):
+        self.assertEqual(self.test_config.db_translate_dict,
+                         {'test': {os.path.abspath(os.getcwd()): os.path.abspath(os.getcwd())}})
+
+    def test_db_name(self):
+        self.assertEqual(self.test_config.db_name, 'test')
+
+    def test_top_path(self):
+        self.assertEqual(self.test_config.top_path(os.path.abspath(os.getcwd()) + '/test'),
+                         os.path.abspath(os.getcwd()))
+
+    def test_resource_paths(self):
+        self.assertEqual(self.test_config.resource_paths, [os.path.abspath(os.getcwd())])
+
+    def test_login_user(self):
+        self.assertEqual(self.test_config.login_user, 'pyiron')
+
+
+if __name__ == '__main__':
+    unittest.main()
