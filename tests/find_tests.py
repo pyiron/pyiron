ab9cf8bb32e5d766f1bb4bf96a0322b81415d009
Jan Janßen <janssen@mpie.de>
2018-04-02 11:47:02 +0200

    find_tests.py: use convert_path()

--- tests/find_tests.py
+++ tests/find_tests.py
@@ -4,6 +4,7 @@ import subprocess
 import sys
 import time
 import numpy as np
+from pyiron_base.core.settings.generic import convert_path
 
 # We would like to use:
 #     python -m unittest discover -s tests -p 'test*.py'
@@ -14,12 +15,9 @@ config_file = os.path.expanduser(os.path.join("~", ".pyiron"))
 if not os.path.exists(config_file):
     with open(config_file, 'w') as cf:
         cf.writelines(['[DEFAULT]\n',
-                       'PROJECT_PATHS = ' + __file__.split('/pyiron_atomistics')[0] + '\n',
-                       'RESOURCE_PATHS = ' + __file__.split('/pyiron_atomistics')[0]
+                       'PROJECT_PATHS = ' + convert_path(__file__).split('/pyiron_atomistics')[0] + '\n',
+                       'RESOURCE_PATHS = ' + convert_path(__file__).split('/pyiron_atomistics')[0]
                        + '/pyiron_atomistics/tests/static\n'])
-print(['[DEFAULT]\n',
-       'PROJECT_PATHS = ' + __file__.split('/pyiron_atomistics')[0] + '\n',
-       'RESOURCE_PATHS = ' + __file__.split('/pyiron_atomistics')[0] + '/pyiron_atomistics/tests/static\n'])
 
 import pyiron_atomistics
 current_file_name = inspect.getfile(inspect.currentframe()).split('/')[-1]
