50124bcc8d4a6ac8bfdf9f5be1d14c187f1f18b2
Jan Janßen <janssen@mpie.de>
2018-04-02 14:28:16 +0200

    Advance to version 0.0.5

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyiron_dft',
-    version='0.0.4',
+    version='0.0.5',
     description='pyiron IDE plugin for DFT',
     long_description='http://pyiron.org',
 
