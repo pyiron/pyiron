c9a5c57d26039e4d8086dbb3c3bf4a64600be40a
Jan Janßen <jan-janssen@users.noreply.github.com>
2018-04-03 12:59:35 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyiron_example_job',
-    version='0.0.3',
+    version='0.0.8',
     description='pyiron IDE plugin for example calculation',
     long_description='http://pyiron.org',
 
@@ -31,6 +31,6 @@ setup(
     ],
 
     keywords='pyiron',
-    packages=find_packages(),
+    packages=find_packages(exclude=["tests"]),
     install_requires=['pyiron_atomistics']
     )
