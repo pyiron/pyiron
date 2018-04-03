e2d7dfe3c483ad299700e5019615e1e5ca0abee2
Jan Janßen <jan-janssen@users.noreply.github.com>
2018-04-03 12:58:19 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
     ],
 
     keywords='pyiron',
-    packages=find_packages(),
+    packages=find_packages(exclude=["tests"]),
     install_requires=['h5io',
                       'h5py',
                       'matplotlib',
