27643bdad541cddde96c25c59945040fba8a64cd
Jan Janßen <jan-janssen@users.noreply.github.com>
2018-04-03 12:59:54 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyiron_atomistics',
-    version='0.0.3',
+    version='0.0.8',
     description='pyiron IDE plugin for atomistic calculations',
     long_description='http://pyiron.org',
 
