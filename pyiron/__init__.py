739fb9c8a8608f16624d3cfaf83cc27046956d75
Jan Janßen <janssen@mpie.de>
2018-04-04 10:13:55 +0200

    Add versioneer again - this time with the right setup.cfg

--- pyiron/__init__.py
+++ pyiron/__init__.py
@@ -3,3 +3,7 @@ __all__ = []
 
 from pyiron.project import Project
 from pyiron_atomistics.structure.atoms import ase_to_pyiron
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
