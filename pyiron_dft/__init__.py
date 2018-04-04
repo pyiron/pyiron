d0ed105ac79daf9584ff3fe62a259b06bea07e45
Jan Janßen <janssen@mpie.de>
2018-04-04 10:32:36 +0200

    Add Versioneer for tracking the version number

--- pyiron_dft/__init__.py
+++ pyiron_dft/__init__.py
@@ -3,3 +3,7 @@ from pyiron_dft.convergence_encut_serial import ConvergenceEncutSerial
 from pyiron_dft.convergence_kpoint_parallel import ConvergenceKpointParallel
 from pyiron_dft.murnaghan_dft import MurnaghanDFT
 __all__ = ['ConvergenceEncutParallel', 'ConvergenceEncutSerial', 'ConvergenceKpointParallel', 'MurnaghanDFT']
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
