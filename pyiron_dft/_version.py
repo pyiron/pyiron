783badee18027ba3496017ba4eb180892c636fc9
Jan Janßen <janssen@mpie.de>
2018-04-05 08:05:22 +0200

    Change from Versioneer Style pep440 to pep440-pre

--- pyiron_dft/_version.py
+++ pyiron_dft/_version.py
@@ -40,7 +40,7 @@ def get_config():
     # _version.py
     cfg = VersioneerConfig()
     cfg.VCS = "git"
-    cfg.style = "pep440"
+    cfg.style = "pep440-pre"
     cfg.tag_prefix = "pyiron_dft-"
     cfg.parentdir_prefix = "pyiron_dft"
     cfg.versionfile_source = "pyiron_dft/_version.py"
