34d0b779c5075461ae20daa63b0422b70f5bce29
Jan Janßen <janssen@mpie.de>
2018-04-05 08:05:22 +0200

    Change from Versioneer Style pep440 to pep440-pre

--- pyiron_atomistics/_version.py
+++ pyiron_atomistics/_version.py
@@ -40,7 +40,7 @@ def get_config():
     # _version.py
     cfg = VersioneerConfig()
     cfg.VCS = "git"
-    cfg.style = "pep440"
+    cfg.style = "pep440-pre"
     cfg.tag_prefix = "pyiron_atomistics-"
     cfg.parentdir_prefix = "pyiron_atomistics"
     cfg.versionfile_source = "pyiron_atomistics/_version.py"
