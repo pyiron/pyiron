726af1aed4dbab3241d3861e831d58653860515d
Jan Janßen <janssen@mpie.de>
2018-04-05 08:05:22 +0200

    Change from Versioneer Style pep440 to pep440-pre

--- pyiron/_version.py
+++ pyiron/_version.py
@@ -40,9 +40,9 @@ def get_config():
     # _version.py
     cfg = VersioneerConfig()
     cfg.VCS = "git"
-    cfg.style = "pep440"
-    cfg.tag_prefix = "'pyiron-'"
-    cfg.parentdir_prefix = "'pyiron'"
+    cfg.style = "pep440-pre"
+    cfg.tag_prefix = "pyiron-"
+    cfg.parentdir_prefix = "pyiron"
     cfg.versionfile_source = "pyiron/_version.py"
     cfg.verbose = False
     return cfg
