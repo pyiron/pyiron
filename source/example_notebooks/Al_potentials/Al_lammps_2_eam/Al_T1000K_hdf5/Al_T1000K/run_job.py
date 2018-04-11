c99323c645a6662660e58203019ab02faac1dd50
Jan Janßen <janssen@mpie.de>
2018-04-11 18:14:23 +0200

    remove output

--- source/example_notebooks/Al_potentials/Al_lammps_2_eam/Al_T1000K_hdf5/Al_T1000K/run_job.py
+++
@@ -1,8 +0,0 @@
-import sys
-from pyiron_base.objects.job.wrapper import JobWrapper
-
-debug = False
-job = JobWrapper(working_directory='/cmmc/u/mpt13/PyIron_data/website/pyiron_docs/source/example_notebooks/Al_potentials/Al_lammps_2_eam/Al_T1000K_hdf5/Al_T1000K',
-                 job_id= 779482 ,
-                 debug=False )
-job.run()
--- source/example_notebooks/first_steps/Al_T600K_hdf5/Al_T600K/run_job.py
+++
@@ -1,8 +0,0 @@
-import sys
-from pyiron_base.objects.job.wrapper import JobWrapper
-
-debug = False
-job = JobWrapper(working_directory='/cmmc/u/mpt13/PyIron_data/website/pyiron_docs/source/example_notebooks/first_steps/Al_T600K_hdf5/Al_T600K',
-                 job_id= 779504 ,
-                 debug=False )
-job.run()
