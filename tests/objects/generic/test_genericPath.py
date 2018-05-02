4ae27634cf451a578ebbe1ce88e3965d9bdc2df8
Jan Janßen <janssen@mpie.de>
2018-05-02 18:43:23 +0200

    Fix test generic path

--- tests/objects/generic/test_genericPath.py
+++ tests/objects/generic/test_genericPath.py
@@ -40,11 +40,11 @@ class TestProject(unittest.TestCase):
         pr_down_two = self.project['../..']
         pr_down_twice = self.project['..']['..']
         self.assertEqual(pr_down_two.__repr__(), pr_down_twice.__repr__())
-        self.assertEqual(str(sorted([directory for directory in os.listdir('.')
-                                     if not os.path.isfile(os.path.join('.', directory))])),
+        self.assertEqual(str({'nodes': [], 'groups': sorted([directory for directory in os.listdir('.')
+                                                             if not os.path.isfile(os.path.join('.', directory))])}),
                          pr_down_one.__repr__())
-        self.assertEqual(str(sorted([directory for directory in os.listdir('..')
-                                     if not os.path.isfile(os.path.join('..', directory))])),
+        self.assertEqual(str({'nodes': [], 'groups': sorted([directory for directory in os.listdir('..')
+                                                             if not os.path.isfile(os.path.join('..', directory))])}),
                          pr_down_two.__repr__())
         self.assertEqual(pr_down_two.__repr__(), pr_down_twice.__repr__())
 
