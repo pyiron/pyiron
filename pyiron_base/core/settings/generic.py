527584fec827d11ac7ce7933b0a2b7494a9be030
Jan Janßen <jan-janssen@users.noreply.github.com>
2018-05-07 12:02:23 +0200

    Update generic.py

--- pyiron_base/core/settings/generic.py
+++ pyiron_base/core/settings/generic.py
@@ -82,10 +82,9 @@ class Settings(with_metaclass(Singleton)):
             user_input = None
             while user_input not in ['yes', 'no']:
                 if sys.version_info.major > 2:
-                    user_input = input('No pyiron installation found, should pyiron be installed [yes/no]:')
+                    user_input = input('It appears that pyiron is not yet configured, do you want to create a default start configuration (recommended: yes). [yes/no]:')
                 else:
-                    user_input = raw_input('It appears that pyiron is not yet configured, do you want to create a default start configuration (recommended: yes). [yes/no] 
-:')
+                    user_input = raw_input('It appears that pyiron is not yet configured, do you want to create a default start configuration (recommended: yes). [yes/no]:')
             if user_input.lower() == 'yes' or user_input.lower() == 'y' or :
                 install_pyiron(config_file_name=config_file,
                                zip_file="resources.zip",
