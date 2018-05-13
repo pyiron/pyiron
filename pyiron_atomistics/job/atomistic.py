15d62be271574c5795c87cd6db08826f89a33b21
sudarsan1989 <surendralal@mpie.de>
2018-05-13 02:17:10 +0200

    AtomisticGenericJob: write_traj function to write the trajectory in any format that ASE also allows

--- pyiron_atomistics/job/atomistic.py
+++ pyiron_atomistics/job/atomistic.py
@@ -2,6 +2,7 @@
 # Copyright (c) Max-Planck-Institut für Eisenforschung GmbH - Computational Materials Design (CM) Department
 # Distributed under the terms of "New BSD License", see the LICENSE file.
 
+from ase.io import write as ase_write
 import copy
 
 import numpy as np
@@ -307,6 +308,25 @@ class AtomisticGenericJob(GenericJobCore):
         """
         return Trajectory(self['output/generic/positions'][::stride], self.structure.get_parent_basis(),
                           center_of_mass=center_of_mass)
+
+    def write_traj(self, filename, format=None, parallel=True, append=False, stride=1, center_of_mass=False, **kwargs):
+        """
+        Writes the trajectory in a given file format based on the `ase.io.write`_ function.
+
+        Args:
+            filename (str): Filename of the output
+            format (str): The specific format of the output
+            parallel (bool):
+            append (bool):
+            stride (int): Writes trajectory every `stride` steps
+            center_of_mass (bool): True if the positions are centered on the COM
+            **kwargs: Additional ase arguments
+
+        .. _ase.io.write: https://wiki.fysik.dtu.dk/ase/_modules/ase/io/formats.html#write
+        """
+        traj = self.trajectory(stride=stride, center_of_mass=center_of_mass)
+        # Using thr ASE output writer
+        ase_write(filename=filename, images=traj, format=format,  parallel=parallel, append=append, **kwargs)
 
     def _run_if_lib_save(self, job_name=None, structure=None, db_entry=True):
         """
