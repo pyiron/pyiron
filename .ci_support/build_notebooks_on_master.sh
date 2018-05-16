a0a62b9a8ec8d2e44b45b5529e0ac39347e01bb4
Jan Janßen <janssen@mpie.de>
2018-05-16 15:30:36 +0200

    echo -e

--- .ci_support/build_notebooks_on_master.sh
+++ .ci_support/build_notebooks_on_master.sh
@@ -4,7 +4,7 @@ if [ "$TRAVIS_BRANCH" = "master" ]; then
     pip install --pre --no-deps pyiron_base pyiron_atomistics pyiron_dft pyiron_example_job pyiron_vasp pyiron
     conda install -y -c conda-forge lammps jupyter 
     git clone https://github.com/pyiron/pyiron-resources.git resources
-    echo "[DEFAULT]\nTOP_LEVEL_DIRS = $(pwd)\nRESOURCE_PATHS = $(pwd)/resources" > ~/.pyiron
+    echo -e "[DEFAULT]\nTOP_LEVEL_DIRS = $(pwd)\nRESOURCE_PATHS = $(pwd)/resources" > ~/.pyiron
     cat ~/.pyiron
     echo $(pwd)
 
