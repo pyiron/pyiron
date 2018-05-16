cbe77a9c71bb652ff468fd41e9f275ae0aaf84c9
Jan Janßen <jan-janssen@users.noreply.github.com>
2018-05-16 16:16:37 +0200

    Update build_notebooks_on_master.sh

--- .ci_support/build_notebooks_on_master.sh
+++ .ci_support/build_notebooks_on_master.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+conda install -y -c conda-forge lammps jupyter nbconvert 
+jupyter kernelspec list
 
 if [ "$TRAVIS_BRANCH" = "master" ] && [ "$PYTHONVER" != "2.7" ]; then
     pip install --pre --no-deps pyiron_base pyiron_atomistics pyiron_dft pyiron_example_job pyiron_vasp pyiron
-    conda install -y -c conda-forge lammps jupyter nbconvert 
+    # conda install -y -c conda-forge lammps jupyter nbconvert 
     git clone https://github.com/pyiron/pyiron-resources.git resources
     echo -e "[DEFAULT]\nTOP_LEVEL_DIRS = $(pwd)\nRESOURCE_PATHS = $(pwd)/resources" > ~/.pyiron
 
