394ec977ce5c320dba3ad7fce71f32b2ac902170
Jan Janßen <janssen@mpie.de>
2018-05-16 15:44:19 +0200

    only python 3

--- .ci_support/build_notebooks_on_master.sh
+++ .ci_support/build_notebooks_on_master.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-if [ "$TRAVIS_BRANCH" = "master" ]; then
+if [ "$TRAVIS_BRANCH" = "master" ] && [ "$PYTHONVER" != "2.7" ]; then
     pip install --pre --no-deps pyiron_base pyiron_atomistics pyiron_dft pyiron_example_job pyiron_vasp pyiron
-    conda install -y -c conda-forge lammps jupyter 
+    conda install -y -c conda-forge lammps jupyter nbconvert 
     git clone https://github.com/pyiron/pyiron-resources.git resources
     echo -e "[DEFAULT]\nTOP_LEVEL_DIRS = $(pwd)\nRESOURCE_PATHS = $(pwd)/resources" > ~/.pyiron
-    cat ~/.pyiron
-    echo $(pwd)
 
     cd notebooks
     for notebook in $(ls *.ipynb); do 
