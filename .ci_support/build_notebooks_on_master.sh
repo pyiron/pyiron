6bcc3bd6d8c055039e98a231c8252b6b4ce235bd
Jan Janßen <janssen@mpie.de>
2018-05-16 15:59:48 +0200

    add Python 2.7

--- .ci_support/build_notebooks_on_master.sh
+++ .ci_support/build_notebooks_on_master.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-if [ "$TRAVIS_BRANCH" = "master" ] && [ "$PYTHONVER" != "2.7" ]; then
+if [ "$TRAVIS_BRANCH" = "master" ]; then
     pip install --pre --no-deps pyiron_base pyiron_atomistics pyiron_dft pyiron_example_job pyiron_vasp pyiron
     conda install -y -c conda-forge lammps jupyter nbconvert 
     git clone https://github.com/pyiron/pyiron-resources.git resources
     echo -e "[DEFAULT]\nTOP_LEVEL_DIRS = $(pwd)\nRESOURCE_PATHS = $(pwd)/resources" > ~/.pyiron
+    python -m ipykernel install --name pytest
 
     cd notebooks
     for notebook in $(ls *.ipynb); do 
-        jupyter nbconvert --ExecutePreprocessor.timeout=9999999 --to notebook --execute $notebook; 
+        jupyter nbconvert --ExecutePreprocessor.timeout=9999999 --ExecutePreprocessor.kernel_name=pytest --to notebook --execute $notebook; 
     done;
 fi;
