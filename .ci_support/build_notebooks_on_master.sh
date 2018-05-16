df829aaf0c07366f70d6792cab22d43bcb12b5b0
Jan Janßen <jan-janssen@users.noreply.github.com>
2018-05-16 16:13:57 +0200

    Update build_notebooks_on_master.sh

--- .ci_support/build_notebooks_on_master.sh
+++ .ci_support/build_notebooks_on_master.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-if [ "$TRAVIS_BRANCH" = "master" ]; then
+if [ "$TRAVIS_BRANCH" = "master" ] && [ "$PYTHONVER" != "2.7" ]; then
     pip install --pre --no-deps pyiron_base pyiron_atomistics pyiron_dft pyiron_example_job pyiron_vasp pyiron
     conda install -y -c conda-forge lammps jupyter nbconvert 
     git clone https://github.com/pyiron/pyiron-resources.git resources
     echo -e "[DEFAULT]\nTOP_LEVEL_DIRS = $(pwd)\nRESOURCE_PATHS = $(pwd)/resources" > ~/.pyiron
-    python -m ipykernel install --name pytest
 
     cd notebooks
     for notebook in $(ls *.ipynb); do 
-        jupyter nbconvert --ExecutePreprocessor.timeout=9999999 --ExecutePreprocessor.kernel_name=pytest --to notebook --execute $notebook; 
+        jupyter nbconvert --ExecutePreprocessor.timeout=9999999 --to notebook --execute $notebook; 
     done;
 fi;
