ad5d196566006fe6f8992f0a8e65ce5131db2e31
Jan Janßen <janssen@mpie.de>
2018-05-16 14:36:44 +0200

    try notebooks for master branch

---
+++ notebooks/build.sh
@@ -0,0 +1,13 @@
+#!/bin/build
+
+if [ "$TRAVIS_BRANCH" = "master" ]; then
+    pip install --pre --no-deps pyiron_base pyiron_atomistics pyiron_dft pyiron_example_job pyiron_vasp pyiron
+    conda install -y -c conda-forge lammps jupyter 
+    git clone https://github.com/pyiron/pyiron-resources.git resources
+    echo "[DEFAULT]\nTOP_LEVEL_DIRS = ${pwd}\nRESOURCE_PATHS = ${pwd}/resources" > ~/.pyiron
+
+    cd notebooks
+    for notebook in $(ls *.ipynb); do 
+        jupyter nbconvert --ExecutePreprocessor.timeout=9999999 --to notebook --execute $notebook; 
+    done;
+fi;
