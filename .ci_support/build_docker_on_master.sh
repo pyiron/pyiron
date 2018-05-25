89f94922733a73f5e940cee34464ae63ec15eee2
Jan Janßen <janssen@mpie.de>
2018-05-25 10:13:01 +0200

    include docker builds

--- .ci_support/build_docker_on_master.sh
+++ .ci_support/build_docker_on_master.sh
@@ -1,7 +1,6 @@
 #!/bin/bash 
 
-if [ "$TRAVIS_BRANCH" = "master" ] && [ "$PYTHONVER" = "3.6" ]; then
-    curl -LO https://raw.github.com/stephanmg/travis-dependent-builds/master/trigger.sh
-    chmod +x trigger.sh
-    ./trigger.sh pyiron pyiron-docker master $TRAVIS_ACCESS_TOKEN 
-fi;
+curl -LO https://raw.github.com/stephanmg/travis-dependent-builds/master/trigger.sh
+chmod +x trigger.sh
+gem install travis
+./trigger.sh pyiron pyiron-docker master $TRAVIS_ACCESS_TOKEN 
