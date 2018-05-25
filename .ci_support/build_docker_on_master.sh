f9fa71845d2786fb8e6c49d8adb79f3410e1ba06
Jan Janßen <jan-janssen@users.noreply.github.com>
2018-05-25 11:31:25 +0200

    Update build_docker_on_master.sh

--- .ci_support/build_docker_on_master.sh
+++ .ci_support/build_docker_on_master.sh
@@ -4,4 +4,4 @@ curl -LO https://raw.githubusercontent.com/stephanmg/travis-dependent-builds/mas
 chmod +x trigger-travis.sh
 gem install websocket -v 1.2.5
 gem install travis
-./trigger-travis.sh pyiron pyiron-docker master ${TRAVIS_ACCESS_TOKEN} "${TRAVIS_COMMIT_MESSAGE}"
+./trigger-travis.sh pyiron pyiron-docker ${TRAVIS_ACCESS_TOKEN} master "${TRAVIS_COMMIT_MESSAGE}"
