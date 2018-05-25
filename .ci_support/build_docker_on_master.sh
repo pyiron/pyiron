aa132c49325be60a68a6141877584fe22b60a285
Jan Janßen <jan-janssen@users.noreply.github.com>
2018-05-25 11:01:06 +0200

    Update build_docker_on_master.sh

--- .ci_support/build_docker_on_master.sh
+++ .ci_support/build_docker_on_master.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 
 curl -LO https://raw.githubusercontent.com/stephanmg/travis-dependent-builds/master/trigger-travis.sh
-chmod +x trigger.sh
+chmod +x trigger-travis.sh
 gem install travis
-./trigger.sh pyiron pyiron-docker master ${TRAVIS_ACCESS_TOKEN} "${TRAVIS_COMMIT_MESSAGE}"
+./trigger-travis.sh pyiron pyiron-docker master ${TRAVIS_ACCESS_TOKEN} "${TRAVIS_COMMIT_MESSAGE}"
