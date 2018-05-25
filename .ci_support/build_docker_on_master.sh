bbdceebe51c855364a0a56d9a083fceb3c9a4861
Jan Janßen <jan-janssen@users.noreply.github.com>
2018-05-25 11:36:26 +0200

    Update build_docker_on_master.sh

--- .ci_support/build_docker_on_master.sh
+++ .ci_support/build_docker_on_master.sh
@@ -1,7 +1,61 @@
-#!/bin/bash 
+#!/bin/bash
+## brief: trigger a downstream travis build
+## see: travis API documentation
 
-curl -LO https://raw.githubusercontent.com/stephanmg/travis-dependent-builds/master/trigger-travis.sh
-chmod +x trigger-travis.sh
+# install
 gem install websocket -v 1.2.5
 gem install travis
-./trigger-travis.sh pyiron pyiron-docker ${TRAVIS_ACCESS_TOKEN} master "${TRAVIS_COMMIT_MESSAGE}"
+
+# variables
+USER="pyiron"
+REPO="pyiron-docker"
+TRAVIS_ACCESS_TOKEN=${GH_TOKEN}
+BRANCH="master"
+MESSAGE="${TRAVIS_COMMIT_MESSAGE}"
+
+# login to travis and get token
+travis login --skip-completion-check --github-token ${TRAVIS_ACCESS_TOKEN}
+travis whoami --skip-completion-check
+TOKEN=$(travis token --skip-completion-check)
+IFS=' ' read -r -a array <<< "$TOKEN"
+TOKEN=${array[${#array[@]}-1]}
+
+# inspired from plume-lib, check arguments and add message
+if [ $# -eq 5 ] ; then
+    MESSAGE=",\"message\": \"$5\""
+elif [ -n "$TRAVIS_REPO_SLUG" ] ; then
+    MESSAGE=",\"message\": \"Triggered from upstream build of $TRAVIS_REPO_SLUG by commit "`git rev-parse --short HEAD`"\""
+fi
+
+# for debugging
+echo "USER=$USER"
+echo "REPO=$REPO"
+echo "TOKEN: ${array[${#array[@]}-1]}"
+echo "BRANCH=$BRANCH"
+echo "MESSAGE=$MESSAGE"
+
+# curl POST request content body
+BODY="{
+  \"request\": {
+  \"branch\":\"$BRANCH\"
+  $MESSAGE
+}}"
+
+# make a POST request with curl (note %2F could be replaced with 
+# / and additional curl arguments, however this works too!)
+curl -s -X POST \
+  -H "Content-Type: application/json" \
+  -H "Accept: application/json" \
+  -H "Travis-API-Version: 3" \
+  -H "Authorization: token ${TOKEN}" \
+  -d "$BODY" \
+  https://api.travis-ci.org/repo/${USER}%2F${REPO}/requests \
+  | tee /tmp/travis-request-output.$$.txt
+
+if grep -q '"@type": "error"' /tmp/travis-request-output.$$.txt; then
+   cat /tmp/travis-request-output.$$.txt
+   exit 1
+elif grep -q 'access denied' /tmp/travis-request-output.$$.txt; then
+   cat /tmp/travis-request-output.$$.txt
+   exit 1
+fi
