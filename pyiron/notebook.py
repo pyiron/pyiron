bdc2ee5e7cb06d9ff08ee8663590d26db6ad5375
Osamu Waseda <samsstud@cmmc001.bc.rzg.mpg.de>
2018-06-05 19:07:46 +0200

    custom_dit as discussed with Joerg

---
+++ pyiron/notebook.py
@@ -0,0 +1,15 @@
+from pyiron_base.objects.generic.hdfio import FileHDFio
+from pyiron_base.objects.generic.parameters import GenericParameters
+from pathlib import Path
+
+class Notebook():
+    def get_custom_dict():
+        folder = Path('.').cwd().parts[-1]
+        hdf_file = Path('.').cwd().parents[1]/folder
+        hdf_file = str(hdf_file)+'.h5'
+        hdf = FileHDFio(hdf_file)
+        custom_dict = GenericParameters()
+        for k,v in zip(hdf[folder+'/input/custom_dict/data_dict']['Parameter'], hdf[folder+'/input/custom_dict/data_dict']['Value']):
+            custom_dict[k] = v
+        return custom_dict
+
