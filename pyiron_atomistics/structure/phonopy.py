ee2f6f525f16663aed176464d1f652744ab1f78a
Osamu Waseda <samsstud@cmmc001.bc.rzg.mpg.de>
2018-09-07 10:13:23 +0200

    tolerance added

--- pyiron_atomistics/structure/phonopy.py
+++ pyiron_atomistics/structure/phonopy.py
@@ -15,10 +15,16 @@ __status__ = "development"
 __date__ = "Sep 1, 2017"
 
 
-def analyse_phonopy_equivalent_atoms(atoms):
+def analyse_phonopy_equivalent_atoms(atoms, symprec=1e-5, angle_tolerance=-1.0):
     """
-    Args:
-        mode (str): ['total', 'numeric', 'str']
+    Args: (read phonopy.structure.spglib for more details)
+        symprec:
+            float: Symmetry search tolerance in the unit of length.
+        angle_tolerance:
+            float: Symmetry search tolerance in the unit of angle deg.
+                If the value is negative, an internally optimized routine
+                is used to judge symmetry.
+
     """
 
     positions = atoms.positions
@@ -31,5 +37,5 @@ def analyse_phonopy_equivalent_atoms(atoms):
     for i in range(3):
         xs = np.dot(np.array(positions), np.linalg.inv(np.array(cell)))
         unitcell = PhonopyAtoms(symbols=types, cell=cell, scaled_positions=xs)
-        ops = spg.get_symmetry(unitcell)
+        ops = spg.get_symmetry(unitcell, symprec=1e-5, angle_tolerance=-1.0)
     return ops['equivalent_atoms']
