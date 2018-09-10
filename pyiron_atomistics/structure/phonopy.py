63c506120360ec2d6bf718f609831dc41af8c72e
Osamu Waseda <samsstud@cmmc001.bc.rzg.mpg.de>
2018-09-10 15:32:39 +0200

    phonopy simplified

--- pyiron_atomistics/structure/phonopy.py
+++ pyiron_atomistics/structure/phonopy.py
@@ -6,13 +6,13 @@ import numpy as np
 from phonopy.structure.atoms import PhonopyAtoms
 import phonopy.structure.spglib as spg
 
-__author__ = "Jan Janssen"
+__author__ = "Osamu Waseda"
 __copyright__ = "Copyright 2017, Max-Planck-Institut für Eisenforschung GmbH - Computational Materials Design (CM) Department"
 __version__ = "1.0"
-__maintainer__ = "Jan Janssen"
-__email__ = "janssen@mpie.de"
+__maintainer__ = "Osamu Waseda"
+__email__ = "waseda@mpie.de"
 __status__ = "development"
-__date__ = "Sep 1, 2017"
+__date__ = "Sep 1, 2018"
 
 
 def analyse_phonopy_equivalent_atoms(atoms, symprec=1e-5, angle_tolerance=-1.0):
@@ -27,15 +27,13 @@ def analyse_phonopy_equivalent_atoms(atoms, symprec=1e-5, angle_tolerance=-1.0):
 
     """
 
-    positions = atoms.positions
+    positions = atoms.scaled_positions
     cell = atoms.cell
     types = atoms.get_chemical_symbols()
     types = list(types)
     natom = len(types)
     positions = np.reshape(np.array(positions), (natom, 3))
     cell = np.reshape(np.array(cell), (3, 3))
-    for i in range(3):
-        xs = np.dot(np.array(positions), np.linalg.inv(np.array(cell)))
-        unitcell = PhonopyAtoms(symbols=types, cell=cell, scaled_positions=xs)
-        ops = spg.get_symmetry(unitcell, symprec=1e-5, angle_tolerance=-1.0)
+    unitcell = PhonopyAtoms(symbols=types, cell=cell, scaled_positions=positions)
+    ops = spg.get_symmetry(unitcell, symprec=1e-5, angle_tolerance=-1.0)
     return ops['equivalent_atoms']
