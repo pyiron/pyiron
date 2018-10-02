29c890eeadd6a57ad162a54efad52c5dc1fc0777
Jan Janßen <janssen@mpie.de>
2018-10-02 17:12:34 +0200

    It was discovered that some projects used assert to enforce interface constraints. However, assert is removed with compiling to optimised byte code (python -o producing *.pyo files). This caused various protections to be removed. The use of assert is also considered as general bad practice in OpenStack codebases.

--- pyiron_lammps/lammps.py
+++ pyiron_lammps/lammps.py
@@ -154,7 +154,8 @@ class Lammps(AtomisticGenericJob):
         Returns:
 
         """
-        assert (self.structure is not None)
+        if not (self.structure is not None):
+            raise AssertionError()
         snapshot = self.structure.copy()
         snapshot.cell = self.get("output/generic/cells")[iteration_step]
         snapshot.positions = self.get("output/generic/positions")[iteration_step]
