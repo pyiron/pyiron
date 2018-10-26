eeb626dd343a76a769a1c3c9d937ca9998ad3ab0
Jan Janßen <janssen@mpie.de>
2018-10-26 17:58:04 +0200

    Fix hdf testing functions

--- tests/test_atoms.py
+++ tests/test_atoms.py
@@ -152,7 +152,7 @@ class TestAtoms(unittest.TestCase):
 
     def test_to_hdf(self):
         if sys.version_info[0] >= 3:
-            filename = os.path.join(os.path.dirname(os.path.abspath(__file__)), "/static/pyiron_atomistics/test_hdf")
+            filename = os.path.join(os.path.dirname(os.path.abspath(__file__)), "static/pyiron_atomistics/test_hdf")
             abs_filename = os.path.abspath(filename)
             hdf_obj = FileHDFio(abs_filename)
             pos, cell = generate_fcc_lattice()
@@ -165,7 +165,7 @@ class TestAtoms(unittest.TestCase):
 
     def test_from_hdf(self):
         if sys.version_info[0] >= 3:
-            filename = os.path.join(os.path.dirname(os.path.abspath(__file__)), "/static/pyiron_atomistics/test_hdf")
+            filename = os.path.join(os.path.dirname(os.path.abspath(__file__)), "static/pyiron_atomistics/test_hdf")
             abs_filename = os.path.abspath(filename)
             hdf_obj = FileHDFio(abs_filename)
             basis = Atoms().from_hdf(hdf_obj, group_name="simple_structure")
