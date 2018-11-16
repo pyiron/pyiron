719968d400ecc6ebcba7ee5e0a59bd2f90383ebb
Jan Janßen <janssen@mpie.de>
2018-11-16 09:46:44 +0100

    Fix import paths

--- pyiron/__init__.py
+++ pyiron/__init__.py
@@ -1,6 +1,10 @@
 __version__ = '0.1'
 __all__ = []
 
+from pyiron.project import Project
+from pyiron.notebook import Notebook
+from pyiron_atomistics.structure.atoms import ase_to_pyiron
+
 from ._version import get_versions
 __version__ = get_versions()['version']
 del get_versions
