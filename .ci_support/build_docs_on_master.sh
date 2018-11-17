83e78d2824468bb9983b7d42323ddcd8b6210e2e
Jan Janßen <janssen@mpie.de>
2018-11-17 07:27:34 +0100

    Update documentation generation

--- .ci_support/build_docs_on_master.sh
+++ .ci_support/build_docs_on_master.sh
@@ -11,15 +11,18 @@ if [ "$TRAVIS_BRANCH" = "master" ]; then
     mkdir pyiron.github.io
     cd pyiron.github.io
     touch .nojekyll
-    echo -e "[DEFAULT]\nPROJECT_PATHS = $PWD\nRESOURCE_PATHS = $PWD/pyiron-resources" > ~/.pyiron
     echo -e "# Build Status\n\nThis repository is automatically updated based on the changes in https://github.com/pyiron/pyiron_docs .\n[![Build Status](https://travis-ci.org/pyiron/pyiron_docs.svg?branch=master)](https://travis-ci.org/pyiron/pyiron_docs)" > README.md
-    cd ../pyiron
+    cd ..
+
+    cd pyiron
     sphinx-apidoc -f -o docs/apidoc pyiron
+    cd docs
+    sphinx-build -b html ./ ../pyiron.github.io
+    cd ..
+
     mkdir uml
     cd uml
-    pyreverse -p ../pyiron -o png pyiron
+    pyreverse -p pyiron -o png ../pyiron
     cp *.png ../../pyiron.github.io
-    cd ../docs
-    sphinx-build -b html ./ ../pyiron.github.io
-    cd ../..
+    cd ..
 fi;
