93785404181b39a02505a2e7f848baf1b6b35858
Jan Janßen <janssen@mpie.de>
2018-11-19 18:07:45 +0100

    Move notebook class to script job

--- pyiron/__init__.py
+++ pyiron/__init__.py
@@ -2,7 +2,7 @@ __version__ = '0.1'
 __all__ = []
 
 from pyiron.project import Project
-from pyiron.notebook import Notebook
+from pyiron.base.objects.job.script import Notebook
 from pyiron.atomistics.structure.atoms import ase_to_pyiron
 
 from ._version import get_versions
--- pyiron/base/objects/job/script.py
+++ pyiron/base/objects/job/script.py
@@ -5,7 +5,9 @@
 from __future__ import print_function
 import os
 import shutil
+from pathlib2 import Path
 from pyiron.base.objects.job.generic import GenericJob
+from pyiron.base.objects.generic.hdfio import FileHDFio
 from pyiron.base.objects.generic.parameters import GenericParameters
 
 
@@ -249,3 +251,32 @@ class ScriptJob(GenericJob):
         return os.path.normpath(os.path.join(os.path.abspath(os.path.curdir), path))
 
 
+class Notebook(object):
+    """
+    class for pyiron notebook objects
+    """
+    @staticmethod
+    def get_custom_dict():
+        folder = Path('.').cwd().parts[-1]
+        hdf_file = Path('.').cwd().parents[1]/folder
+        hdf_file = str(hdf_file)+'.h5'
+        if Path(hdf_file).exists():
+            hdf = FileHDFio(hdf_file)
+            custom_dict = GenericParameters()
+            for k, v in zip(hdf[folder+'/input/custom_dict/data_dict']['Parameter'],
+                            hdf[folder+'/input/custom_dict/data_dict']['Value']):
+                custom_dict[k] = v
+            return custom_dict
+        else:
+            print(hdf_file, 'not found')
+            return None
+
+    @staticmethod
+    def store_custom_output_dict(output_dict):
+        folder = Path('.').cwd().parts[-1]
+        hdf_file = Path('.').cwd().parents[1] / folder
+        hdf_file = str(hdf_file) + '.h5'
+        hdf = FileHDFio(hdf_file)
+        hdf[folder].create_group('output')
+        for k, v in output_dict.items():
+            hdf[folder + '/output'][k] = v
