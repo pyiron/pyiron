984b4da9d586343f70b0b840496057c0764921d6
Jan Janßen <janssen@mpie.de>
2018-12-07 09:01:21 +0100

    Implement new job interfaces for example job

---
+++ pyiron/testing/interface.py
@@ -0,0 +1,111 @@
+import numpy as np
+import posixpath
+from pyiron.base.job.interface import JobInterface, InteractiveInterface
+from pyiron.base.pyio.parser import Logstatus
+
+
+class RandomInterface(JobInterface):
+    @staticmethod
+    def write_input(job):
+        """
+        Call routines that generate the codespecifc input files
+        """
+        job.input.write_file(file_name="input.inp", cwd=job.working_directory)
+
+    def collect_output(self, job):
+        """
+        Parse the output files of the example job and store the results in the HDF5 File.
+        """
+        self.collect_output_log(project_hdf5=job.project_hdf5,
+                                working_directory=job.working_directory,
+                                file_name="output.log")
+        self.collect_warnings(project_hdf5=job.project_hdf5,
+                              working_directory=job.working_directory,
+                              file_name="info.log")
+
+    @staticmethod
+    def collect_output_log(project_hdf5, working_directory, file_name="output.log"):
+        """
+        general purpose routine to extract output from logfile
+
+        Args:
+            project_hdf5:
+            working_directory:
+            file_name (str): output.log - optional
+        """
+        tag_dict = {"alat": {"arg": "0", "rows": 0},
+                    "count": {"arg": "0", "rows": 0},
+                    "energy": {"arg": "0", "rows": 0}}
+        lf = Logstatus()
+        file_name = posixpath.join(working_directory, file_name)
+        lf.extract_file(file_name=file_name, tag_dict=tag_dict)
+        with project_hdf5.open("output/generic") as h5:
+            lf.to_hdf(h5)
+            h5["energy_tot"] = np.array(h5["energy"])
+            h5["volume"] = np.array(h5["alat"])
+
+    @staticmethod
+    def collect_warnings(project_hdf5, working_directory, file_name="info.log"):
+        """
+        Collect the warnings if any were written to the info.log file and store them in the HDF5 file
+        """
+        warnings_lst = []
+        with open(posixpath.join(working_directory, file_name), "r") as f:
+            lines = f.readlines()
+        for line in lines:
+            if "WARNING" in line:
+                warnings_lst.append(line.split("WARNING"))
+                warnings_lst[-1][-1] = warnings_lst[-1][-1].rstrip()
+        if len(warnings_lst) > 0:
+            warnings_dict = {'Module': [warnings_lst[i][0] for i in range(len(warnings_lst))],
+                             'Message': [warnings_lst[i][1] for i in range(len(warnings_lst))]}
+            print("module: ", warnings_lst[:][:])
+            with project_hdf5.open("output") as hdf_output:
+                hdf_output["WARNINGS"] = warnings_dict
+
+    @staticmethod
+    def collect_logfiles(job):
+        """
+        Collect the errors from the info.log file and store them in the HDF5 file
+        """
+        errors_lst = []
+        with open(posixpath.join(job.working_directory, "info.log"), "r") as f:
+            lines = f.readlines()
+        for line in lines:
+            if "ERROR" in line:
+                errors_lst.append(line)
+        if len(errors_lst) > 0:
+            with job.project_hdf5.open("output") as hdf_output:
+                hdf_output["ERRORS"] = errors_lst
+
+
+class InteractiveRandomInterface(RandomInterface, InteractiveInterface):
+    def __init__(self, job):
+        job._interactive_cache = {'alat': [], 'count': [], 'energy': []}
+
+    @staticmethod
+    def run_if_interactive(job):
+        """
+        Run the job as Python library and store the result in the HDF5 File.
+
+        Returns:
+            int: job ID
+        """
+        from pyiron.testing.executable import ExampleExecutable
+        job.status.running = True
+        alat, count, energy = ExampleExecutable().run_lib(job.input)
+        job._interactive_cache['alat'].append(alat)
+        job._interactive_cache['count'].append(count)
+        job._interactive_cache['energy'].append(energy)
+
+    @staticmethod
+    def interactive_close(job):
+        job.to_hdf()
+        with job.project_hdf5.open("output") as h5:
+            h5["generic/energy"] = np.array(job._interactive_cache['energy'])
+            h5["generic/volume"] = np.array(job._interactive_cache['alat'])
+            h5["generic/alat"] = np.array(job._interactive_cache['alat'])
+            h5["generic/count"] = np.array(job._interactive_cache['count'])
+            h5["generic/energy_tot"] = np.array(job._interactive_cache['energy'])
+        job.project.db.item_update(job._runtime(), job._job_id)
+        job.status.finished = True
--- pyiron/testing/randomatomistic.py
+++ pyiron/testing/randomatomistic.py
@@ -3,13 +3,11 @@
 # Distributed under the terms of "New BSD License", see the LICENSE file.
 
 from __future__ import print_function
-import numpy as np
 import os
-import posixpath
 from pyiron.base.generic.parameters import GenericParameters
 from pyiron.base.job.generic import GenericJob
-from pyiron.base.pyio.parser import Logstatus
 from pyiron.atomistics.job.atomistic import AtomisticGenericJob
+from pyiron.testing.interface import InteractiveRandomInterface
 
 """
 Example Job class for testing the pyiron classes  
@@ -121,73 +119,7 @@ class ExampleJob(GenericJob):
         self.input = ExampleInput()
         self.executable = "python " + str(os.path.dirname(os.path.realpath(__file__))) + \
                           "/executable.py"
-        self._interactive_cache = {'alat': [], 'count': [], 'energy': []}
-
-    # define routines that create all necessary input files
-    def write_input(self):
-        """
-        Call routines that generate the codespecifc input files
-        """
-        self.input.write_file(file_name="input.inp", cwd=self.working_directory)
-
-    # define routines that collect all output files
-    def collect_output(self):
-        """
-        Parse the output files of the example job and store the results in the HDF5 File.
-        """
-        self.collect_output_log()
-        self.collect_warnings()
-        self.collect_logfiles()
-
-    def collect_output_log(self, file_name="output.log"):
-        """
-        general purpose routine to extract output from logfile
-
-        Args:
-            file_name (str): output.log - optional
-        """
-        tag_dict = {"alat": {"arg": "0", "rows": 0},
-                    "count": {"arg": "0", "rows": 0},
-                    "energy": {"arg": "0", "rows": 0}}
-        lf = Logstatus()
-        file_name = posixpath.join(self.working_directory, file_name)
-        lf.extract_file(file_name=file_name, tag_dict=tag_dict)
-        with self.project_hdf5.open("output/generic") as h5:
-            lf.to_hdf(h5)
-            h5["energy_tot"] = np.array(h5["energy"])
-            h5["volume"] = np.array(h5["alat"])
-
-    def collect_warnings(self):
-        """
-        Collect the warnings if any were written to the info.log file and store them in the HDF5 file
-        """
-        warnings_lst = []
-        with open(posixpath.join(self.working_directory, "info.log"), "r") as f:
-            lines = f.readlines()
-        for line in lines:
-            if "WARNING" in line:
-                warnings_lst.append(line.split("WARNING"))
-                warnings_lst[-1][-1] = warnings_lst[-1][-1].rstrip()
-        if len(warnings_lst) > 0:
-            warnings_dict = {'Module': [warnings_lst[i][0] for i in range(len(warnings_lst))],
-                             'Message': [warnings_lst[i][1] for i in range(len(warnings_lst))]}
-            print("module: ", warnings_lst[:][:])
-            with self.project_hdf5.open("output"):
-                self._hdf5["WARNINGS"] = warnings_dict
-
-    def collect_logfiles(self):
-        """
-        Collect the errors from the info.log file and store them in the HDF5 file
-        """
-        errors_lst = []
-        with open(posixpath.join(self.working_directory, "info.log"), "r") as f:
-            lines = f.readlines()
-        for line in lines:
-            if "ERROR" in line:
-                errors_lst.append(line)
-        if len(errors_lst) > 0:
-            with self.project_hdf5.open("output") as hdf_output:
-                hdf_output["ERRORS"] = errors_lst
+        self._interface = InteractiveRandomInterface(job=self)
 
     def to_hdf(self, hdf=None, group_name=None):
         """
@@ -212,31 +144,6 @@ class ExampleJob(GenericJob):
         super(ExampleJob, self).from_hdf(hdf=hdf, group_name=group_name)
         with self.project_hdf5.open("input") as hdf5_input:
             self.input.from_hdf(hdf5_input)
-
-    def run_if_interactive(self):
-        """
-        Run the job as Python library and store the result in the HDF5 File.
-
-        Returns:
-            int: job ID
-        """
-        from pyiron.testing.executable import ExampleExecutable
-        self.status.running = True
-        alat, count, energy = ExampleExecutable().run_lib(self.input)
-        self._interactive_cache['alat'].append(alat)
-        self._interactive_cache['count'].append(count)
-        self._interactive_cache['energy'].append(energy)
-
-    def interactive_close(self):
-        self.to_hdf()
-        with self.project_hdf5.open("output") as h5:
-            h5["generic/energy"] = np.array(self._interactive_cache['energy'])
-            h5["generic/volume"] = np.array(self._interactive_cache['alat'])
-            h5["generic/alat"] = np.array(self._interactive_cache['alat'])
-            h5["generic/count"] = np.array(self._interactive_cache['count'])
-            h5["generic/energy_tot"] = np.array(self._interactive_cache['energy'])
-        self.project.db.item_update(self._runtime(), self._job_id)
-        self.status.finished = True
 
 
 class ExampleInput(GenericParameters):
