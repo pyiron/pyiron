dd680dc3ececa4a4363c9c8dca25fc8a40384aad
Jan Janßen <janssen@mpie.de>
2018-12-11 12:07:41 +0100

    murnint - remove create_jobs()

--- pyiron/atomistics/master/murninterface.py
+++ pyiron/atomistics/master/murninterface.py
@@ -15,31 +15,20 @@ __date__ = "Sep 1, 2017"
 
 
 class MurnaghanInt(Murnaghan):
-    def create_jobs(self):
+    def run_static(self):
         if self.ref_job.server.run_mode.interactive:
             ham = self.ref_job.copy()
-            basis_ref = ham.structure
-
-            vol_min = 1 - self.input['vol_range']
-            vol_max = 1 + self.input['vol_range']
-
-            self.submission_status.submitted_jobs = 0
             ham.master_id = self.job_id
-            for strain in np.linspace(vol_min, vol_max, self.input['num_points']):
-                strain = np.round(strain, 7)  # prevent too long job_names
-                self._logger.debug("Murnaghan child project {}".format(self.project.__str__()))
-                basis = basis_ref.copy()
-                basis.set_cell(basis.cell * strain ** (1. / 3.), scale_atoms=True)
-                ham.structure = basis
+
+            for strain in self._job_generator.parameter_list:
+                ham = self._job_generator.modify_job(job=ham, parameter=strain)
                 ham.run()
-                self._logger.info('Murnaghan: finished job {}'.format(ham.job_name))
-                self.ref_job.structure = basis_ref
-            self.structure = basis_ref
+
             ham.interactive_close()
             self.status.collect = True
             self.run()
         else:
-            super(MurnaghanInt, self).create_jobs()
+            super(MurnaghanInt, self).run_static()
 
     def collect_output(self):
         erg_lst, vol_lst, err_lst, id_lst = [], [], [], []
