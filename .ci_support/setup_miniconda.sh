d029a4d2a9312d192a39858110ccdf6cb67d2444
Jan Janßen <jan-janssen@users.noreply.github.com>
2019-01-04 12:43:48 +0100

    Add conda-forge channel for building conda package

--- .ci_support/setup_miniconda.sh
+++ .ci_support/setup_miniconda.sh
@@ -5,6 +5,7 @@ conda info -a
 conda config --set always_yes yes --set changeps1 no
 conda update -q conda
 conda config --set anaconda_upload yes
-conda install -y -c conda-forge python=${2} conda-build conda-verify anaconda-client future psutil pytables numpy matplotlib scipy sqlalchemy pathlib2 pandas h5py coveralls coverage ase spglib h5io phonopy
+conda config --add channels conda-forge
+conda install -y python=${2} conda-build conda-verify anaconda-client future psutil pytables numpy matplotlib scipy sqlalchemy pathlib2 pandas h5py coveralls coverage ase spglib h5io phonopy
 pip install --pre .
 echo -e "[DEFAULT]\nTOP_LEVEL_DIRS = $PWD\nRESOURCE_PATHS = $PWD/tests/static" > ~/.pyiron
