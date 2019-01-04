5a2a983fa11877f862f19af9db803b5cc8022f6f
Jan Janßen <jan-janssen@users.noreply.github.com>
2019-01-04 11:45:09 +0100

    Update setup_miniconda.sh

--- .ci_support/setup_miniconda.sh
+++ .ci_support/setup_miniconda.sh
@@ -4,10 +4,7 @@ bash miniconda.sh -b -p $HOME/miniconda
 conda info -a
 conda config --set always_yes yes --set changeps1 no
 conda update -q conda
-conda install python=${2} conda-build conda-verify anaconda-client psutil pytables numpy matplotlib scipy sqlalchemy pathlib2 pandas h5py
-conda config --add channels conda-forge
-conda config --add channels pyiron
 conda config --set anaconda_upload yes
-conda install -y -c conda-forge python=${2} coveralls coverage ase spglib h5io phonopy
+conda install -y -c conda-forge python=${2} conda-build conda-verify anaconda-client psutil pytables numpy matplotlib scipy sqlalchemy pathlib2 pandas h5py coveralls coverage ase spglib h5io phonopy
 pip install --pre .
 echo -e "[DEFAULT]\nTOP_LEVEL_DIRS = $PWD\nRESOURCE_PATHS = $PWD/tests/static" > ~/.pyiron
