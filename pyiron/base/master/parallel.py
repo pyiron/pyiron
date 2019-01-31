9722987da71f7b8da22e37aae257f858b29d86ba
Jan Janßen <jan-janssen@users.noreply.github.com>
2019-01-31 19:52:46 +0100

    Update parallel.py

--- pyiron/base/master/parallel.py
+++ pyiron/base/master/parallel.py
@@ -2,7 +2,7 @@
 # Copyright (c) Max-Planck-Institut für Eisenforschung GmbH - Computational Materials Design (CM) Department
 # Distributed under the terms of "New BSD License", see the LICENSE file.
 
-from __future__ import print_function
+from __future__ import division, print_function
 from collections import OrderedDict
 from datetime import datetime
 import numpy as np
