5e5879c361b8f06c9917fff3caa62892b8788191
Jan Janßen <janssen@mpie.de>
2019-02-06 09:36:26 +0100

    Python 2.7 compatibility

--- tests/base/job/test_jobtype.py
+++ tests/base/job/test_jobtype.py
@@ -4,9 +4,9 @@ from pyiron.base.job.jobtype import static_isinstance
 
 class TestJobType(unittest.TestCase):
     def test_static_isinstance(self):
-        self.assertTrue(static_isinstance(obj=list(), obj_type=['builtins.list', 'builtins.object']))
-        self.assertTrue(static_isinstance(obj=list(), obj_type='builtins.list'))
-        self.assertTrue(static_isinstance(obj=list().__class__, obj_type='builtins.list'))
+        self.assertTrue(static_isinstance(obj=list(), obj_type=['builtins.list', '__builtin__.list']))
+        self.assertTrue(any[static_isinstance(obj=list(), obj_type='builtins.list'),
+                            static_isinstance(obj=list(), obj_type='__builtin__.list')])
         self.assertRaises(TypeError, static_isinstance, list(), 1)
 
 
