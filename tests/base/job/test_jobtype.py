20cb71e1ac7f0da8e66c98c46e02acd71a11328b
Jan Janßen <janssen@mpie.de>
2019-02-06 08:53:53 +0100

    fix test

--- tests/base/job/test_jobtype.py
+++ tests/base/job/test_jobtype.py
@@ -7,8 +7,7 @@ class TestJobType(unittest.TestCase):
         self.assertTrue(static_isinstance(obj=list(), obj_type=['builtins.list', 'builtins.object']))
         self.assertTrue(static_isinstance(obj=list(), obj_type='builtins.list'))
         self.assertTrue(static_isinstance(obj=list().__class__, obj_type='builtins.list'))
-        with self.assertRaises(TypeError) as context:
-            self.assertTrue(static_isinstance(obj=list(), obj_type=1))
+        self.assertRaises(TypeError, static_isinstance, list(), 1)
 
 
 if __name__ == '__main__':
