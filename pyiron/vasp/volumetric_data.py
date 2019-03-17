0587d9a32f95edd86e433f5fdb3ac418a89a9f3d
sudarsan1989 <surendralal@mpie.de>
2019-03-17 06:47:03 +0100

    Cleaner and faster reading of volumetric data

--- pyiron/vasp/volumetric_data.py
+++ pyiron/vasp/volumetric_data.py
@@ -117,54 +117,71 @@ class VaspVolumetricData(VolumetricData):
             self.atoms = atoms
             self._total_data = data["total"]
 
-    @staticmethod
-    def _read_vol_data(filename, normalize=True):
+    def _read_vol_data(self, filename, normalize=True):
         with open(filename, "r") as f:
             struct_lines = list()
             get_grid = False
             n_x = 0
             n_y = 0
             n_z = 0
+            n_grid = 0
+            n_grid_str = None
+            total_data_list = list()
+            atoms = None
             for i, line in enumerate(f):
                 strip_line = line.strip()
                 if not get_grid:
                     if strip_line == "":
                         get_grid = True
                     struct_lines.append(strip_line)
-                else:
+                elif n_grid_str is None:
                     n_x, n_y, n_z = [int(val) for val in strip_line.split()]
-                    break
-            if (n_x * n_y * n_z) >= 1:
-                with warnings.catch_warnings(record=True) as w:
-                    warnings.simplefilter("always")
-                    load_txt = np.genfromtxt(f, invalid_raise=False)
-                    if issubclass(w[-1].category, np.lib.npyio.ConversionWarning):
-                        s = Settings()
-                        s.logger.warning("File:" + filename + "numpy ConversionWarning ignored delibrately ")
-                    else:
-                        warnings.warn(w[-1].message, w[-1].category)
-                total_data = np.zeros((n_x, n_y, n_z))
-                all_data = np.hstack(load_txt)
-                all_indices = np.arange(len(all_data), dtype=int)
-                x_indices = all_indices % n_x
-                y_indices = all_indices / n_x % n_y
-                y_indices = np.array(y_indices, dtype=int)
-                z_indices = all_indices / (n_x * n_y)
-                z_indices = np.array(z_indices, dtype=int)
-                total_data[x_indices, y_indices, z_indices] = all_data
-                try:
-                    atoms = atoms_from_string(struct_lines)
-                except ValueError:
-                    pot_str = filename.split("/")
-                    pot_str[-1] = "POTCAR"
-                    potcar_file = "/".join(pot_str)
-                    species = get_species_list_from_potcar(potcar_file)
-                    atoms = atoms_from_string(struct_lines, species_list=species)
-                if normalize:
-                    total_data /= 1
-                return atoms, total_data
-            else:
-                raise ValueError("Unable to get the grid size")
+                    n_grid = n_x * n_y * n_z
+                    n_grid_str = " ".join([str(val) for val in [n_x, n_y, n_z]])
+                    with warnings.catch_warnings(record=True) as w:
+                        warnings.simplefilter("always")
+                        load_txt = np.genfromtxt(f, max_rows=int(n_grid / 5), invalid_raise=False)
+                        if len(w) > 0:
+                            if issubclass(w[-1].category, np.lib.npyio.ConversionWarning):
+                                s = Settings()
+                                s.logger.warning("File:" + filename + "numpy ConversionWarning ignored deliberately")
+                            else:
+                                warnings.warn(w[-1].message, w[-1].category)
+                    total_data = self._fastest_index_reshape(load_txt, [n_x, n_y, n_z])
+                    try:
+                        atoms = atoms_from_string(struct_lines)
+                    except ValueError:
+                        pot_str = filename.split("/")
+                        pot_str[-1] = "POTCAR"
+                        potcar_file = "/".join(pot_str)
+                        species = get_species_list_from_potcar(potcar_file)
+                        atoms = atoms_from_string(struct_lines, species_list=species)
+                    if normalize:
+                        total_data /= atoms.get_volume()
+                    total_data_list.append(total_data)
+                elif atoms is not None:
+                    grid_str = n_grid_str.replace(" ", "")
+                    if grid_str == strip_line.replace(" ", ""):
+                        load_txt = np.genfromtxt(f, max_rows=int(n_grid / 5), invalid_raise=False)
+                        total_data = self._fastest_index_reshape(load_txt, [n_x, n_y, n_z])
+                        if normalize:
+                            total_data /= atoms.get_volume()
+                        total_data_list.append(total_data)
+            return atoms, total_data_list
+
+    @staticmethod
+    def _fastest_index_reshape(raw_data, grid):
+        n_x, n_y, n_z = grid
+        total_data = np.zeros((n_x, n_y, n_z))
+        all_data = np.hstack(raw_data)
+        all_indices = np.arange(len(all_data), dtype=int)
+        x_indices = all_indices % n_x
+        y_indices = all_indices / n_x % n_y
+        y_indices = np.array(y_indices, dtype=int)
+        z_indices = all_indices / (n_x * n_y)
+        z_indices = np.array(z_indices, dtype=int)
+        total_data[x_indices, y_indices, z_indices] = all_data
+        return total_data
 
     @property
     def total_data(self):
