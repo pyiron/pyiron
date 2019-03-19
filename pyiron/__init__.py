5d2882b21affdca236451fdd5673bcf46a7777c8
Jan Janßen <janssen@mpie.de>
2019-03-19 10:20:07 +0100

    implement: from pyiron import pyiron_to_ase

--- pyiron/__init__.py
+++ pyiron/__init__.py
@@ -2,7 +2,7 @@ __version__ = '0.1'
 __all__ = []
 
 from pyiron.project import Project
-from pyiron.atomistics.structure.atoms import ase_to_pyiron
+from pyiron.atomistics.structure.atoms import ase_to_pyiron, pyiron_to_ase
 from pyiron.base.job.script import Notebook
 
 from ._version import get_versions
