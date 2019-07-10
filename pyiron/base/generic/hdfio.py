a8b0f68fc0d2b88d0b90aac02f8d61b098d4a85a
Felix <lochner@mpie.de>
2019-07-10 14:38:01 +0200

    modified exclude_nodes and exclude_groups to include full directories as 'output/generic'

--- pyiron/base/generic/hdfio.py
+++ pyiron/base/generic/hdfio.py
@@ -575,24 +575,29 @@ class FileHDFio(object):
         exclude_groups_split = [i.split('/', 1) for i in exclude_groups]
         exclude_nodes_split = [i.split('/', 1) for i in exclude_nodes]
         for p in hdf_old.list_nodes():
-            for ex in exclude_nodes_split:
-                if p not in ex[0]:
-                    hdf_new[p] = hdf_old[p]
-                elif not len(ex) == 1:
-                    hdf_new[p] = hdf_old[p]
+            if len(exclude_nodes_split) == 0:
+                hdf_new[p] = hdf_old[p]
+            else:
+                for ex in exclude_nodes_split:
+                    if p not in ex[0]:
+                        hdf_new[p] = hdf_old[p]
+                    elif not len(ex) == 1:
+                        hdf_new[p] = hdf_old[p]
         for p in hdf_old.list_groups():
-            for ex in exclude_groups_split:
-                if p not in ex[0]:
-                    h_new = hdf_new.create_group(p)
-                    ex_n = [e[-1] for e in exclude_nodes_split if p == e[0] or len(e) == 1]
-                    self.hd_copy(hdf_old[p], h_new, exclude_groups=exclude_groups, exclude_nodes=ex_n)
-                elif not len(ex) == 1:
-                    h_new = hdf_new.create_group(p)
-                    ex_n = [e[-1] for e in exclude_nodes_split if
-                                     p == e[0] or len(e) == 1]
-                    ex_g = [e[-1] for e in exclude_groups_split if
-                                     p == e[0] or len(e) == 1]
-                    self.hd_copy(hdf_old[p], h_new, exclude_groups=ex_g, exclude_nodes=ex_n)
+            if len(exclude_groups_split) == 0:
+                h_new = hdf_new.create_group(p)
+                self.hd_copy(hdf_old[p], h_new, exclude_nodes=exclude_nodes)
+            else:
+                for ex in exclude_groups_split:
+                    if p not in ex[0]:
+                        h_new = hdf_new.create_group(p)
+                        ex_n = [e[-1] for e in exclude_nodes_split if p == e[0] or len(e) == 1]
+                        self.hd_copy(hdf_old[p], h_new, exclude_groups=exclude_groups, exclude_nodes=ex_n)
+                    elif not len(ex) == 1:
+                        h_new = hdf_new.create_group(p)
+                        ex_n = [e[-1] for e in exclude_nodes_split if p == e[0] or len(e) == 1]
+                        ex_g = [e[-1] for e in exclude_groups_split if p == e[0] or len(e) == 1]
+                        self.hd_copy(hdf_old[p], h_new, exclude_groups=ex_g, exclude_nodes=ex_n)
         ### old ###
 #        for p in hdf_old.list_nodes():
 #            if p not in exclude_nodes:
