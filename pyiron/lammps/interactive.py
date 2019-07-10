a5af7e29296960779426f422d4d6abc8552f7f70
sudarsan1989 <surendralal@mpie.de>
2019-07-10 19:13:19 +0200

    Adding angles for the water molecules

--- pyiron/lammps/interactive.py
+++ pyiron/lammps/interactive.py
@@ -311,8 +311,14 @@ class LammpsInteractive(LammpsBase, GenericInteractive):
         # self._interactive_lib_command("kspace_style " + self.input.potential["kspace_style"])
         self._interactive_lib_command("create_bonds many Oatoms H1atoms 1 0.7 1.4")
         self._interactive_lib_command("create_bonds many Oatoms H2atoms 1 0.7 1.4")
+        for i in range(len(o_indices)):
+            self._interactive_lib_command("create_bonds single/angle 1 {} {} {}".format(
+                int(h1_indices) + 1, int(o_indices) + 1, int(h2_indices) + 1))
         self._interactive_lib_command("pair_style " + self.input.potential["pair_style"])
-        self._interactive_lib_command("pair_coeff * * 0.0 0.0")
+        values = np.array(self.input.potential._dataset['Value'])
+        pair_val = values[["pair_coeff" in val for val in self.input.potential._dataset['Parameter']]]
+        for val in pair_val:
+            self._interactive_lib_command("pair_coeff " + val)
         self._interactive_lib_command("kspace_style " + self.input.potential["kspace_style"])
 
     def from_hdf(self, hdf=None, group_name=None):
