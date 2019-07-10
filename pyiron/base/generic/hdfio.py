b38b3d4b77cb72d310d807aa39f863cfbe7d1ebd
Felix <lochner@mpie.de>
2019-07-10 15:55:13 +0200

    debug

--- pyiron/base/generic/hdfio.py
+++ pyiron/base/generic/hdfio.py
@@ -585,12 +585,16 @@ class FileHDFio(object):
                         hdf_new[p] = hdf_old[p]
         for p in hdf_old.list_groups():
             if len(exclude_groups_split) == 0:
+                print("Checkpoint 1")
                 h_new = hdf_new.create_group(p)
                 ex_n = [e[-1] for e in exclude_nodes_split if p == e[0] or len(e) == 1]
                 self.hd_copy(hdf_old[p], h_new, exclude_nodes=ex_n)
             else:
+                print("Checkpoint 2")
                 for ex in exclude_groups_split:
+                    print("Checkpoint 3, group: ", ex)
                     if p not in ex[0]:
+                        print("Checkpoint 4, group: ", ex[0])
                         h_new = hdf_new.create_group(p)
                         ex_n = [e[-1] for e in exclude_nodes_split if p == e[0] or len(e) == 1]
                         self.hd_copy(hdf_old[p], h_new, exclude_groups=exclude_groups, exclude_nodes=ex_n)
@@ -598,6 +602,7 @@ class FileHDFio(object):
                         h_new = hdf_new.create_group(p)
                         ex_n = [e[-1] for e in exclude_nodes_split if p == e[0] or len(e) == 1]
                         ex_g = [e[-1] for e in exclude_groups_split if p == e[0] or len(e) == 1]
+                        print("Checkpoint 5, group: ", ex_n, ex_g)
                         self.hd_copy(hdf_old[p], h_new, exclude_groups=ex_g, exclude_nodes=ex_n)
         ### old ###
         # for p in hdf_old.list_nodes():
