28d05b8f5a9f76b51ee8f0e4cdb291a4d3f808f3
Ahmed-Aslam <aaahmedaslam@gmail.com>
2019-07-11 16:27:39 +0200

    print pressure

--- pyiron/lammps/base.py
+++ pyiron/lammps/base.py
@@ -430,9 +430,11 @@ class LammpsBase(AtomisticGenericJob):
             del lf.status_dict['memory']
         with self.project_hdf5.open("output/generic") as hdf_output:
             # This is a hack for backward comparability
-            print('Line 433',lf.status_dict['pressures'])
+            print('Line 433',lf.status_dict['pressures'][0])
             if "temperature" in lf.status_dict.keys():
                 hdf_output["temperatures"] = np.array(lf.status_dict["temperature"])[0][1]
+            #hdf_output["temperatures"] = hdf_output["temperatures"]
+            print()
             lf.to_hdf(hdf_output)
 
     def calc_minimize(self, e_tol=0.0, f_tol=1e-2, max_iter=100000, pressure=None, n_print=100):
