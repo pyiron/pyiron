8f9671b184f0e0b57d16d8ae862b34ccc7218c40
Ahmed-Aslam <aaahmedaslam@gmail.com>
2019-07-16 15:52:08 +0200

    Unsorted_Sorted list_versions

--- pyiron/base/job/executable.py
+++ pyiron/base/job/executable.py
@@ -204,8 +204,7 @@ class Executable(object):
                             executable[len("run_" + self.__name__) + 1:-len(extension)] not in executable_dict.keys():
                         executable_dict[executable[len("run_" + self.__name__) + 1:-len(extension)]] = \
                             os.path.join(path, executable).replace('\\', '/')
-                        print('Executables_names',self.__name__)
-            print('Executable',executable_dict)
+                 print('Executable',executable)
             return executable_dict
         except OSError:  # No executable exists - This is the case for GenericJob and other abstract job classes.
             return dict()
