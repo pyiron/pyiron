f7ddccca5cc45bb793e4e933b07074be77c3bc83
Osamu Waseda <waseda@mpie.de>
2019-07-23 21:24:52 +0200

    workaround

--- pyiron/base/generic/parameters.py
+++ pyiron/base/generic/parameters.py
@@ -537,15 +537,21 @@ class GenericParameters(PyironObject):
             tab_dict["Parameter"] = ["" for _ in tab_dict["Value"]]
 
         string_lst = []
-        for par, _, c in zip(tab_dict["Parameter"], tab_dict["Value"], tab_dict["Comment"]):
-            v = self[par]
-            # special treatment for values that are bool or str
-            if isinstance(v, bool):
-                v_str = self._bool_dict[v]
-            elif isinstance(v, str):  # TODO: introduce variable for string symbol (" or ')
+        for par, v, c in zip(tab_dict["Parameter"], tab_dict["Value"], tab_dict["Comment"]):
+            if self.val_only:
                 v_str = v
+            # special treatment for values that are bool or str
             else:
-                v_str = str(v)
+                try:
+                    v = self[par]
+                    if isinstance(v, bool):
+                        v_str = self._bool_dict[v]
+                    elif isinstance(v, str):  # TODO: introduce variable for string symbol (" or ')
+                        v_str = v
+                    else:
+                        v_str = str(v)
+                except:
+                    v_str = v
 
             if par == "Comment":
                 string_lst.append(str(v) + self.end_value_char + "\n")
