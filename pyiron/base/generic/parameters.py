4fb933ca5ecff11555b83654eac88fcc71a2371e
Jan Janssen <jan-janssen@users.noreply.github.com>
2019-07-26 12:16:26 +0200

    Update parameters.py

--- pyiron/base/generic/parameters.py
+++ pyiron/base/generic/parameters.py
@@ -539,17 +539,13 @@ class GenericParameters(PyironObject):
 
         string_lst = []
         for par, v, c in zip(tab_dict["Parameter"], tab_dict["Value"], tab_dict["Comment"]):
-            if not self.val_only:
-                v = self[par]
-                # special treatment for values that are bool or str
-                if isinstance(v, bool):
-                    v_str = self._bool_dict[v]
-                elif isinstance(v, str):  # TODO: introduce variable for string symbol (" or ')
-                    v_str = v
-                else:
-                    v_str = str(v)
-            else:
+            # special treatment for values that are bool or str
+            if isinstance(v, bool):
+                v_str = self._bool_dict[v]
+            elif isinstance(v, str):  # TODO: introduce variable for string symbol (" or ')
                 v_str = v
+            else:
+                v_str = str(v)
 
             if par == "Comment":
                 string_lst.append(str(v) + self.end_value_char + "\n")
