764350707dda838b437ba3620bcd0870a6ceb755
Jan Janßen <janssen@mpie.de>
2019-07-27 14:16:54 +0200

    Enforce string for python 2.7

--- pyiron/atomistics/structure/periodic_table.py
+++ pyiron/atomistics/structure/periodic_table.py
@@ -59,7 +59,7 @@ class ChemicalElement(object):
         self.el = None
 
     def _init_mendeleev(self, element_str):
-        self._mendeleev_element = element(element_str)
+        self._mendeleev_element = element(str(element_str))
         self._mendeleev_property_lst = [s for s in dir(self._mendeleev_element) if not s.startswith('_')]
 
     def __getattr__(self, item):
