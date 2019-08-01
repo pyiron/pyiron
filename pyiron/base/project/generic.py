e67abb24356e6c75f0f9fa828b1d291c490e3527
Felix <lochner@mpie.de>
2019-08-01 17:36:06 +0200

    pugfix in 'get_jobs_status'

--- pyiron/base/project/generic.py
+++ pyiron/base/project/generic.py
@@ -472,8 +472,8 @@ class Project(ProjectPath):
             prints an overview of the job status.
         """
         df = job_table(database=self.db, sql_query=self.sql_query, user=self.user, project_path=self.project_path,
-                       recursive=recursive, columns=["status"], element_lst=element_lst)
-        return df.value_counts()
+                       recursive=recursive, element_lst=element_lst)
+        return df['status'].value_counts()
 
     def keys(self):
         """
