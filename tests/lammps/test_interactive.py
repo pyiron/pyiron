ca4d1240bccdb0a1f2a4866c1d97bffef8b737f8
Osamu Waseda <waseda@mpie.de>
2019-08-08 08:51:04 +0200

    more tests

--- tests/lammps/test_interactive.py
+++ tests/lammps/test_interactive.py
@@ -10,26 +10,25 @@ import ase.units as units
 
 class InteractiveLibrary(object):
     def __init__(self):
-        self._command = None
+        self._command = []
 
     def command(self, command_in):
-        self._command = command_in
-
-    def _get_lib_command(self):
-        return self._command
+        self._command.append(command_in)
 
     def scatter_atoms(self, *args):
-        self._command = ' '.join([str(arg) for arg in args])
+        self._command.append(' '.join([str(arg) for arg in args]))
 
 
 
 class TestLammps(unittest.TestCase):
+    def setUp(self):
+        self.job._interactive_library = InteractiveLibrary()
+
     @classmethod
     def setUpClass(cls):
         cls.execution_path = os.path.dirname(os.path.abspath(__file__))
         cls.project = Project(os.path.join(cls.execution_path, 'lammps'))
         cls.job = Lammps(project=ProjectHDFio(project=cls.project, file_name='lammps'), job_name='lammps')
-        cls.job._interactive_library = InteractiveLibrary()
         cls.job.server.run_mode.interactive = True
         cls.job.structure = Atoms(symbols='Fe2', positions=np.outer(np.arange(2), np.ones(3)), cell=2*np.eye(3))
 
@@ -42,12 +41,23 @@ class TestLammps(unittest.TestCase):
 
     def test_interactive_cells_setter(self):
         self.job.interactive_cells_setter(np.eye(3))
-        self.assertEqual(self.job._interactive_library._get_lib_command(),
+        self.assertEqual(self.job._interactive_library._command[-1],
                          'change_box all x final 0 1.000000 y final 0 1.000000 z final 0 1.000000 units box')
 
     def test_interactive_positions_setter(self):
         self.job.interactive_positions_setter(np.arange(6).reshape(2,3))
-        self.assertEqual(self.job._interactive_library._get_lib_command(), 'change_box all remap')
+        self.assertTrue(self.job._interactive_library._command[0].startswith('x 1 3'))
+        self.assertEqual(self.job._interactive_library._command[1], 'change_box all remap')
+
+    def test_interactive_execute(self):
+        self.job._interactive_lammps_input()
+        self.assertEqual(self.job._interactive_library._command,
+                         ['fix ensemble all nve',
+                          'variable dumptime equal 100',
+                          'thermo_style custom step temp pe etotal pxx pxy pxz pyy pyz pzz vol',
+                          'thermo_modify format float %20.15g',
+                          'thermo 100'])
+
 
 if __name__ == '__main__':
     unittest.main()
