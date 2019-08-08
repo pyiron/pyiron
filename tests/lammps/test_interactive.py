1a7728f14e1d5ac5ea71beb285fd123d5fc10717
Osamu Waseda <waseda@mpie.de>
2019-08-08 08:36:00 +0200

    lammps interactive class added in unittests

---
+++ tests/lammps/test_interactive.py
@@ -0,0 +1,53 @@
+import unittest
+import numpy as np
+import os
+from pyiron.base.project.generic import Project
+from pyiron.atomistics.structure.atoms import Atoms
+from pyiron.base.generic.hdfio import ProjectHDFio
+from pyiron.lammps.lammps import Lammps
+from pyiron.lammps.base import LammpsStructure
+import ase.units as units
+
+class InteractiveLibrary(object):
+    def __init__(self):
+        self._command = None
+
+    def command(self, command_in):
+        self._command = command_in
+
+    def _get_lib_command(self):
+        return self._command
+
+    def scatter_atoms(self, *args):
+        self._command = ' '.join([str(arg) for arg in args])
+
+
+
+class TestLammps(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.execution_path = os.path.dirname(os.path.abspath(__file__))
+        cls.project = Project(os.path.join(cls.execution_path, 'lammps'))
+        cls.job = Lammps(project=ProjectHDFio(project=cls.project, file_name='lammps'), job_name='lammps')
+        cls.job._interactive_library = InteractiveLibrary()
+        cls.job.server.run_mode.interactive = True
+        cls.job.structure = Atoms(symbols='Fe2', positions=np.outer(np.arange(2), np.ones(3)), cell=2*np.eye(3))
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.execution_path = os.path.dirname(os.path.abspath(__file__))
+        project = Project(os.path.join(cls.execution_path, 'lammps'))
+        project.remove_jobs(recursive=True)
+        project.remove(enable=True)
+
+    def test_interactive_cells_setter(self):
+        self.job.interactive_cells_setter(np.eye(3))
+        self.assertEqual(self.job._interactive_library._get_lib_command(),
+                         'change_box all x final 0 1.000000 y final 0 1.000000 z final 0 1.000000 units box')
+
+    def test_interactive_positions_setter(self):
+        self.job.interactive_positions_setter(np.arange(6).reshape(2,3))
+        self.assertEqual(self.job._interactive_library._get_lib_command(), 'change_box all remap')
+
+if __name__ == '__main__':
+    unittest.main()
