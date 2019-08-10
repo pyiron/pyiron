0a08730879ae6a83b7feda6df0b7c5a1cef88e66
Jan Janßen <janssen@mpie.de>
2019-08-10 10:16:46 +0200

    Enforce first letter as capital on windows

--- .ci_support/pyironconfig.py
+++ .ci_support/pyironconfig.py
@@ -5,6 +5,9 @@ def main():
     current_path = os.path.abspath(os.path.curdir)
     top_level_path = current_path.replace('\\', '/')
     resource_path = os.path.join(current_path, "tests", "static").replace('\\', '/')
+    if os.name == 'nt':
+        top_level_path = top_level_path[0].upper() + top_level_path[1:]
+        resource_path = resource_path[0].upper() + resource_path[1:]
     pyiron_config = os.path.expanduser('~/.pyiron').replace('\\', '/')
     if not os.path.exists(pyiron_config):
         with open(pyiron_config, 'w') as f:
