70f78fbf875249195520215bf62f965d016f8178
Jan Janßen <janssen@mpie.de>
2019-08-10 08:19:20 +0200

    use central config script

--- .ci_support/setup_miniconda.sh
+++ .ci_support/setup_miniconda.sh
@@ -4,4 +4,3 @@ bash miniconda.sh -b -p $HOME/miniconda
 conda info -a
 conda config --set always_yes yes --set changeps1 no
 conda update -q conda
-echo -e "[DEFAULT]\nTOP_LEVEL_DIRS = $PWD\nRESOURCE_PATHS = $PWD/tests/static" > ~/.pyiron
