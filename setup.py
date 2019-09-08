98d031edef677dbbc5bd19677bc2acc1c3037231
Jan Janßen <janssen@mpie.de>
2019-09-08 16:31:59 +0200

    Add pyfileindex to requirements

--- setup.py
+++ setup.py
@@ -42,6 +42,7 @@ setup(
                       'pathlib2',
                       'phonopy',
                       'psutil',
+                      'pyfileindex',
                       'pysqa',
                       'scipy',
                       'six',
