ffc7c78f178786e4c2b1f089c4524ac94b7f1200
Sam Waseda <o.waseda@mpie.de>
2019-09-11 15:21:42 +0200

    stress order corrected

--- pyiron/lammps/interactive.py
+++ pyiron/lammps/interactive.py
@@ -563,23 +563,16 @@ class LammpsInteractive(LammpsBase, GenericInteractive):
             self._interactive_lib_command("compute st all stress/atom NULL")
             self._interactive_lib_command("run 0")
             self.interactive_cache["stress"] = []
-        ss = (
-            np.array(
-                [
-                    self._interactive_library.extract_compute("st", 1, 2)[i][
-                        j + (j != k) * (k + 2)
-                    ]
-                    for i in range(len(self.structure))
-                    for j in range(3)
-                    for k in range(3)
-                ]
-            ).reshape(len(self.structure), 3, 3)
-            / 1.602e6
-        )
+        ID = self._interactive_library.extract_atom("id", 0)
+        ID = np.array([ID[i] for i in range(len(self.structure))])-1
+        ID = np.arange(len(ID))[np.argsort(ID)]
+        ind = np.array([0, 3, 4, 3, 1, 5, 4, 5, 2])
+        ss = self._interactive_library.extract_compute("st", 1, 2)
+        ss = np.array([ss[i][j] for i in range(len(self.structure)) for j in range(6)]).reshape(-1, 6)[ID]
+        ss = ss[:, ind].reshape(len(self.structure), 3, 3)/1.602e6
         if np.matrix.trace(self._interactive_prism.R) != 3:
-            ss = np.dot(
-                np.dot(self._interactive_prism.R, ss), self._interactive_prism.R.T
-            )
+            ss = np.einsum('ij,njk->nik', self._interactive_prism.R, ss)
+            ss = np.einsum('nij,kj->nik', ss, self._interactive_prism.R)
         return ss
 
     def interactive_pressures_getter(self):
