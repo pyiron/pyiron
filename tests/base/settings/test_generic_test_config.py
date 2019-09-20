196bf984adb14f7554b577c88ce05d3c048ce091
Jan Janßen <janssen@mpie.de>
2019-09-20 21:49:17 +0200

    Change tests to have consistent object

--- tests/base/settings/test_generic_test_config.py
+++ tests/base/settings/test_generic_test_config.py
@@ -22,8 +22,8 @@ class TestConfigSettingsStatic(unittest.TestCase):
         cls.test_config = Settings(
             config={
                 "sql_file": "sqlite.db",
-                "project_paths": os.path.join(cls.resource_path, "../../../../.."),
-                "resource_paths": os.path.join(cls.resource_path, "../../../../static"),
+                "project_paths": os.path.join(cls.resource_path, "../../../.."),
+                "resource_paths": os.path.join(cls.resource_path, "../../../static"),
             }
         )
 
@@ -60,7 +60,7 @@ class TestConfigSettingsStatic(unittest.TestCase):
             self.test_config.resource_paths,
             [
                 os.path.abspath(
-                    os.path.join(self.resource_path, "../../../../static")
+                    os.path.join(self.resource_path, "../../../static")
                 ).replace("\\", "/")
             ],
         )
