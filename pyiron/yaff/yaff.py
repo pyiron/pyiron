c42f473861aaf5cfe37ac48b659f06691befefb7
Sander Borgmans <sander.borgmans@ugent.be>
2019-10-14 13:11:06 +0200

    Datatype for list of strings in h5 file, attempt 2

--- pyiron/yaff/yaff.py
+++ pyiron/yaff/yaff.py
@@ -357,7 +357,7 @@ class Yaff(AtomisticGenericJob):
             self.structure.to_hdf(hdf5_input)
             self.input.to_hdf(hdf5_input)
             hdf5_input['generic/jobtype'] = self.jobtype
-            hdf5_input['generic/ffatypes'] = self.ffatypes.astype(h5py.special_dtype(vlen=str)) # in h5py 2.10.0 this changes to "h5py.string_dtype(encoding='utf-8')"
+            hdf5_input['generic/ffatypes'] = np.asarray(self.ffatypes,'S22')
             hdf5_input['generic/ffatype_ids'] = self.ffatype_ids
 
     def from_hdf(self, hdf=None, group_name=None):
@@ -366,7 +366,7 @@ class Yaff(AtomisticGenericJob):
             self.input.from_hdf(hdf5_input)
             self.structure = Atoms().from_hdf(hdf5_input)
             self.jobtype = hdf5_input['generic/jobtype']
-            self.ffatypes = hdf5_input['generic/ffatypes']
+            self.ffatypes = np.asarray(hdf5_input['generic/ffatypes'],'S22')
             self.ffatype_ids = hdf5_input['generic/ffatype_ids']
 
     def get_structure(self, iteration_step=-1, wrap_atoms=True):
