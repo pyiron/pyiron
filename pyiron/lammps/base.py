f4a6ac2a096d153cb5821bc0dfe2536e7b708b78
Liam Huber <liam.huber@gmail.com>
2019-10-31 15:57:10 +0100

    Accommodate higher dimensional index arrays

--- pyiron/lammps/base.py
+++ pyiron/lammps/base.py
@@ -411,11 +411,10 @@ class LammpsBase(AtomisticGenericJob):
         Returns:
             numpy.ndarray: Those integers mapped onto the first N integers.
         """
-        print(indices)
         original_indices = np.array(indices, dtype=int)
         unique_indices = np.unique(original_indices)  # Unique and sorted!
         for n, unique_i in enumerate(unique_indices):
-            indices[np.argwhere(original_indices == unique_i).reshape(-1)] = n
+            indices[np.argwhere(original_indices == unique_i)] = n
 
         return indices
 
