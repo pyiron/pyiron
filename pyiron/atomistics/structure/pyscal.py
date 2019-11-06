0a2573d34b962ee3e5424955db292f97fb128fd1
Jan Janssen <jan-janssen@users.noreply.github.com>
2019-11-06 11:12:39 +0100

    Update pyscal.py

--- pyiron/atomistics/structure/pyscal.py
+++ pyiron/atomistics/structure/pyscal.py
@@ -18,7 +18,7 @@ __status__ = "development"
 __date__ = "Nov 6, 2019"
 
 
-def _get_steinhardt_parameter(cell, positions, cutoff=3.50, n_clusters=2):
+def _get_steinhardt_parameter(cell, positions, cutoff=3.50, n_clusters=2, q=[4, 6]):
     sys = pc.System()
     prism = UnfoldingPrism(cell, digits=15)
     xhi, yhi, zhi, xy, xz, yz = prism.get_lammps_prism_str()
@@ -26,20 +26,20 @@ def _get_steinhardt_parameter(cell, positions, cutoff=3.50, n_clusters=2):
     sys.box = [[0.0, float(xhi)], [0.0, float(yhi)], [0.0, float(zhi)]]
     sys.atoms = [pc.Atom(pos=p, id=i) for i, p in enumerate(coords)]
     sys.find_neighbors(method='cutoff', cutoff=cutoff)
-    sys.calculate_q([4, 6], averaged=True)
-    sysq = sys.get_qvals([4, 6], averaged=True)
+    sys.calculate_q(q, averaged=True)
+    sysq = sys.get_qvals(q, averaged=True)
     cl = cluster.KMeans(n_clusters=n_clusters)
     ind = cl.fit(list(zip(*sysq))).labels_ == 0
     return sysq, ind
 
 
-def get_steinhardt_parameter_job(job, cutoff=3.50, n_clusters=2):
+def get_steinhardt_parameter_job(job, cutoff=3.50, n_clusters=2, q=[4, 6]):
     cell = job['output/generic/cells'][-1]
     positions = job['output/generic/positions'][-1]
-    return _get_steinhardt_parameter(cell=cell, positions=positions, cutoff=cutoff, n_clusters=n_clusters)
+    return _get_steinhardt_parameter(cell=cell, positions=positions, cutoff=cutoff, n_clusters=n_clusters, q=q)
 
 
-def get_steinhardt_parameter_structure(structure, cutoff=3.50, n_clusters=2):
+def get_steinhardt_parameter_structure(structure, cutoff=3.50, n_clusters=2, q=[4, 6]):
     cell = structure.cell.copy()
     positions = structure.positions.copy()
-    return _get_steinhardt_parameter(cell=cell, positions=positions, cutoff=cutoff, n_clusters=n_clusters)
+    return _get_steinhardt_parameter(cell=cell, positions=positions, cutoff=cutoff, n_clusters=n_clusters, q=q)
