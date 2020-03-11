0075b26136b3721cf56db28a6eb685ba5d1086d9
Jan Janssen <jan-janssen@users.noreply.github.com>
2020-03-11 10:09:44 +0100

    Update setup_pyiron.sh

--- .ci_support/setup_pyiron.sh
+++ .ci_support/setup_pyiron.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-conda install -y -c conda-forge python=${1} ase=3.17 coveralls coverage defusedxml=0.5.0 dill=0.3.0 future=0.17.1 gitpython=2.1.11 h5io=0.1.1 h5py=2.9.0 matplotlib=2.2.4 mendeleev=0.5.1 molmod=1.4.5 numpy=1.16.4 pandas=0.24.2 pathlib2=2.3.4 phonopy=2.3.2 psutil=5.6.3 pyfileindex=0.0.4 pysqa=0.0.7 scipy=1.2.1 six=1.12.0 spglib=1.14.1 sqlalchemy=1.3.8 pytables=3.5.1 tamkin=1.2.6 tqdm=4.35.0 yaff=1.2.4
+conda install -y -c conda-forge python=${1} ase=3.17 coveralls coverage defusedxml=0.5.0 dill=0.3.0 future=0.17.1 gitpython=2.1.11 h5io=0.1.1 h5py=2.9.0 matplotlib=2.2.4 mendeleev=0.5.1 molmod=1.4.5 numpy=1.16.4 pandas=0.24.2 pathlib2=2.3.4 phonopy=2.3.2 psutil=5.6.3 pyfileindex=0.0.4 pysqa=0.0.7 scipy=1.2.1 six=1.12.0 spglib=1.14.1 sqlalchemy=1.3.8 pytables=3.5.1 tamkin=1.2.6 tqdm=4.35.0 yaff=1.4.2
 pip install --pre .
