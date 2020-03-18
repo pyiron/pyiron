0276be362647f4c2393ac8608f4687c13cc8f486
Sander Borgmans <sander.borgmans@ugent.be>
2020-03-18 11:08:02 +0100

    Try clearing all imported modules at the start?

--- tests/gaussian/test_gaussian.py
+++ tests/gaussian/test_gaussian.py
@@ -2,6 +2,9 @@
 # Copyright (c) Max-Planck-Institut für Eisenforschung GmbH - Computational Materials Design (CM) Department
 # Distributed under the terms of "New BSD License", see the LICENSE file.
 
+import sys
+sys.modules.clear(  )
+
 import unittest
 import os
 import posixpath
