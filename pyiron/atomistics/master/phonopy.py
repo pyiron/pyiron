bebc5eb8b85b6cb1850c5692482305b5e6d96433
Jan Janssen <jan-janssen@users.noreply.github.com>
2020-05-08 22:36:09 +0200

    Phonopy fix

--- pyiron/atomistics/master/phonopy.py
+++ pyiron/atomistics/master/phonopy.py
@@ -234,9 +234,10 @@ class PhonopyJob(AtomisticParallelMaster):
                 "output/generic/forces"
             ]
         else:
+            pr_job = self.project_hdf5.open(self.job_name)
             forces_lst = [
-                self.project_hdf5.inspect(job_id)["output/generic/forces"][-1]
-                for job_id in self._get_jobs_sorted()
+                pr_job.inspect(job_name)["output/generic/forces"][-1]
+                for job_name in self._get_jobs_sorted()
             ]
         self.phonopy.set_forces(forces_lst)
         self.phonopy.produce_force_constants()
