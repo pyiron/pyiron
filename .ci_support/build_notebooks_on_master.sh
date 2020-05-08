3b55929850a3d4d3863abd77adde162333abd883
Jan Janssen <jan-janssen@users.noreply.github.com>
2020-05-08 20:17:51 +0200

    Update build_notebooks_on_master.sh

--- .ci_support/build_notebooks_on_master.sh
+++ .ci_support/build_notebooks_on_master.sh
@@ -8,6 +8,12 @@ else
 fi;
 
 # execute notebooks
+i = 0;
 for notebook in $(ls notebooks/*.ipynb); do 
-    papermill ${notebook} ${notebook%.*}-out.${notebook##*.} -k $kernel || travis_terminate 1;
+    papermill ${notebook} ${notebook%.*}-out.${notebook##*.} -k $kernel || i=$((i+1));
 done;
+
+# push error to next level
+if [ $i -gt 0 ]; then
+    exit 1;
+fi;
