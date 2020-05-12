2e3cdb15d56b47feae423175f61c68e006f3c029
Marvin Poul <ponder@creshal.de>
2020-05-12 15:12:30 +0200

    conserve species order
    `get_species_symbols()` returns a sorted list, but
    `species_translate_list` should follow the original order of
    `Structure.species`.

--- pyiron/lammps/structure.py
+++ pyiron/lammps/structure.py
@@ -403,9 +403,10 @@ class LammpsStructure(GenericParameters):
         # extract electric charges from potential file
         q_dict = {}
         species_translate_list = []
-        for el in self.structure.get_species_symbols():
-            q_dict[el] = self.potential.get_charge(el)
-            species_translate_list.append(self.potential.get_element_id(el))
+        for species in self.structure.species:
+            species_name = species.Abbreviation
+            q_dict[species_name] = self.potential.get_charge(species_name)
+            species_translate_list.append(self.potential.get_element_id(species_name))
         sorted_species_list = np.array(self._potential.get_element_lst())
         molecule_lst, bonds_lst, angles_lst = [], [], []
         bond_type_lst, angle_type_lst = [], []
