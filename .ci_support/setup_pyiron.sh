20189c9b11d2b7e38f91e88ff9c4a8db05ae7e38
Jan Janßen <janssen@mpie.de>
2020-05-30 16:52:08 +0200

    Merge setup scripts

--- .ci_support/setup_pyiron.sh
+++ .ci_support/setup_pyiron.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
-conda install python=${1}
+# Setup Miniconda
+wget ${1} -O miniconda.sh
+bash miniconda.sh -b -p $HOME/miniconda
+conda info -a
+conda config --set always_yes yes --set changeps1 no
+
+# Setup pyiron
+conda install python=${2}
 conda env update --name root --file .ci_support/environment.yml
 pip install --pre .
