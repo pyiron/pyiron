2920f97adbfb21fc138035327cc8a3ec1c713ca6
Marvin Poul <ponder@creshal.de>
2020-06-17 09:34:45 +0200

    use double quotes

--- pyiron/base/generic/inputlist.py
+++ pyiron/base/generic/inputlist.py
@@ -10,8 +10,8 @@ def _normalize(key):
     if isinstance(key, str):
         if key.isdecimal():
             return int(key)
-        elif '/' in key:
-            return tuple(key.split('/'))
+        elif "/" in key:
+            return tuple(key.split("/"))
 
     elif isinstance(key, tuple) and len(key) == 1:
         return _normalize(key[0])
@@ -21,14 +21,14 @@ def _normalize(key):
 
 
 class InputList(MutableMapping):
-    '''
+    """
     Mutable sequence with optional keys.
 
     If no argument is given, the constructor creates a new empty InputList.  If
     specified init maybe a Sequence, Set or Mapping and all recursive
     occurrences of these are also wrapped by InputList.
     >>> pl = InputList([3, 2, 1, 0])
-    >>> pm = InputList({'foo': 24, 'bar': 42})
+    >>> pm = InputList({"foo": 24, "bar": 42})
 
     Access can be like a normal list with integers or optionally with strings
     as keys.
@@ -36,14 +36,14 @@ class InputList(MutableMapping):
     3
     >>> pl[2]
     1
-    >>> pm['foo']
+    >>> pm["foo"]
     24
 
     Keys do not have to be present for all elements.
     >>> pl2 = InputList([1,2])
-    >>> pl2['end'] = 3
+    >>> pl2["end"] = 3
     >>> pl2
-    InputList({0: 1, 1: 2, 'end': 3})
+    InputList({0: 1, 1: 2, "end": 3})
 
     It is also allowed to set an item one past the length of the InputList,
     this is then equivalent to appending that element.  This allows to use the
@@ -62,26 +62,26 @@ class InputList(MutableMapping):
     24
     >>> pm.append = 23
     >>> pm
-    InputList({'foo': 24, 'bar': 42, 'append': 23})
+    InputList({"foo": 24, "bar": 42, "append": 23})
 
     Keys and indices can be tuples to traverse nested InputLists.
-    >>> pn = InputList({'foo': {'bar': [4, 2]}})
-    >>> pn['foo', 'bar']
+    >>> pn = InputList({"foo": {"bar": [4, 2]}})
+    >>> pn["foo", "bar"]
     InputList([4, 2])
-    >>> pn['foo', 'bar', 0]
+    >>> pn["foo", "bar", 0]
     4
 
-    Using keys with '/' in them is equivalent to the above after splitting the
+    Using keys with "/" in them is equivalent to the above after splitting the
     key.
-    >>> pn['foo/bar'] == pn['foo', 'bar']
+    >>> pn["foo/bar"] == pn["foo", "bar"]
     True
-    >>> pn['foo/bar/0'] == pn['foo', 'bar', 0]
+    >>> pn["foo/bar/0"] == pn["foo", "bar", 0]
     True
 
     To make that work strings that are only decimal digits are automatically
     converted to integers before accessing the list and keys are restricted to
     not only contain digits on initialization.
-    >>> pl['0'] == pl[0]
+    >>> pl["0"] == pl[0]
     True
     >>> InputList({1: 42})
     Traceback (most recent call last):
@@ -93,9 +93,9 @@ class InputList(MutableMapping):
     When initializing from a dict, it may not have integers or decimal strings
     as keys unless they match their position in the insertion order.  This is
     to avoid ambiguities in the final order of the InputList.
-    >>> InputList({0: 'foo', 1: 'bar', 2: 42})
-    InputList(['foo', 'bar', 42])
-    >>> InputList({0: 'foo', 2: 42, 1: 'bar'})
+    >>> InputList({0: "foo", 1: "bar", 2: 42})
+    InputList(["foo", "bar", 42])
+    >>> InputList({0: "foo", 2: 42, 1: "bar"})
     Traceback (most recent call last):
         File "<stdin>", line 1, in <module>
         File "proplist.py", line 132, in __init__
@@ -109,7 +109,7 @@ class InputList(MutableMapping):
     normal list and discard the keys use `values()`.
     >>> pm[0]
     24
-    >>> pn['0/0/1']
+    >>> pn["0/0/1"]
     2
     >>> list(pl)
     [0, 1, 2, 3]
@@ -117,7 +117,7 @@ class InputList(MutableMapping):
     [3, 2, 1, 0]
     >>> list(pl.keys())
     [0, 1, 2, 3]
-    '''
+    """
 
     __version__ = "0.1.0"
 
@@ -127,9 +127,9 @@ class InputList(MutableMapping):
         # setting these immediately after object creation ensures that they are
         # always defined and attribute access works even before __init__ is
         # called.  This is relevant on deepcopy & pickling.
-        object.__setattr__(instance, '_store', [])
-        object.__setattr__(instance, '_indices', {})
-        object.__setattr__(instance, 'table_name', None)
+        object.__setattr__(instance, "_store", [])
+        object.__setattr__(instance, "_indices", {})
+        object.__setattr__(instance, "table_name", None)
 
         return instance
 
@@ -163,7 +163,7 @@ class InputList(MutableMapping):
             try:
                 return self._store[key]
             except IndexError:
-                raise IndexError('list index out of range') from None
+                raise IndexError("list index out of range") from None
         elif isinstance(key, str):
 
             try:
@@ -172,7 +172,7 @@ class InputList(MutableMapping):
                 raise KeyError(repr(key)) from None
         else:
             raise ValueError(
-                    '{} is not a valid key, must be str or int'.format(key)
+                    "{} is not a valid key, must be str or int".format(key)
             )
 
     def __setitem__(self, key, val):
@@ -196,7 +196,7 @@ class InputList(MutableMapping):
                 self._store[self._indices[key]] = val
         else:
             raise ValueError(
-                    '{} is not a valid key, must be str or int'.format(key)
+                    "{} is not a valid key, must be str or int".format(key)
             )
 
     def __delitem__(self, key):
@@ -219,7 +219,7 @@ class InputList(MutableMapping):
                     self._indices[k] = i - 1
         else:
             raise ValueError(
-                    '{} is not a valid key, must be str or int'.format(key)
+                    "{} is not a valid key, must be str or int".format(key)
             )
 
     def __getattr__(self, name):
@@ -250,13 +250,13 @@ class InputList(MutableMapping):
     def __repr__(self):
         name = self.__class__.__name__
         if self.has_keys():
-            return name + '({' + \
-                    ', '.join('{!r}: {!r}'.format(k, v) for k, v in self.items()) + \
-            '})'
+            return name + "({" + \
+                    ", ".join("{!r}: {!r}".format(k, v) for k, v in self.items()) + \
+            "})"
         else:
-            return name + '([' + \
-                    ', '.join('{!r}'.format(v) for v in self._store) + \
-            '])'
+            return name + "([" + \
+                    ", ".join("{!r}".format(v) for v in self._store) + \
+            "])"
 
     @classmethod
     def _wrap_val(cls, val):
@@ -266,13 +266,13 @@ class InputList(MutableMapping):
             return val
 
     def to_builtin(self, stringify = False):
-        '''
-        Convert the list back to builtin dict's and list's recursively.
+        """
+        Convert the list back to builtin dict"s and list"s recursively.
 
         Args:
             stringify (bool, optinal): convert all non-recursive elements to
             str
-        '''
+        """
 
         if self.has_keys():
             dd = {}
@@ -297,7 +297,7 @@ class InputList(MutableMapping):
                             if isinstance(v, InputList) else v
                                 for v in self.values())
 
-    # allows 'nice' displays in jupyter notebooks
+    # allows "nice" displays in jupyter notebooks
     def _repr_json_(self):
         return self.to_builtin(stringify = True)
 
@@ -323,7 +323,7 @@ class InputList(MutableMapping):
             return super().get(key, default = default)
 
     def update(self, init, wrap = False, **kwargs):
-        '''
+        """
         Add all elements or key-value pairs from init to this list.  If wrap is
         not given, behaves as the generic method.
 
@@ -332,7 +332,7 @@ class InputList(MutableMapping):
             wrap (bool): if True wrap all encountered Sequences and Mappings in
                         InputLists recursively
             **kwargs: update from this mapping as well
-        '''
+        """
         if wrap:
             if isinstance(init, (Sequence, Set)):
                 for v in init:
@@ -347,13 +347,13 @@ class InputList(MutableMapping):
                             self.append(v)
                         else:
                             raise ValueError(
-                                'keys in initializer must not be int or str of '
-                                'decimal digits or in correct order, '
-                                'is {!r}'.format(k))
+                                "keys in initializer must not be int or str of "
+                                "decimal digits or in correct order, "
+                                "is {!r}".format(k))
                     else:
                         self[k] = v
             else:
-                ValueError('init must be Sequence, Set or Mapping')
+                ValueError("init must be Sequence, Set or Mapping")
 
             for k in kwargs:
                 self[k] = kwargs[k]
@@ -361,27 +361,27 @@ class InputList(MutableMapping):
             super().update(init, **kwargs)
 
     def append(self, val):
-        '''
+        """
         Add new value to the list without a key.
 
         Args:
             val: new element
-        '''
+        """
         self._store.append(self._wrap_val(val))
 
     def extend(self, vals):
-        '''
+        """
         Append vals to the end of this InputList.
 
         Args:
             vals (Sequence): any python sequence to draw new elements from
-        '''
+        """
 
         for v in vals:
             self.append(v)
 
     def insert(self, index, val, key = None):
-        '''
+        """
         Add a new element to the list at the specified position, with an optional
         key.  If the key is already in the list it will be updated to point to
         the new element at the new index.  If index is larger than list, append
@@ -391,7 +391,7 @@ class InputList(MutableMapping):
             index (int):            place val after this element
             val:                    new element to add
             key (str, optional):    optional key to mark the new element
-        '''
+        """
         if key != None:
             for k, i in self._indices.items():
                 if i >= index:
@@ -401,7 +401,7 @@ class InputList(MutableMapping):
         self._store.insert(index, val)
 
     def mark(self, index, key):
-        '''
+        """
         Add a key to an existing item at index.  If key already exists, it is
         overwritten.
 
@@ -413,12 +413,12 @@ class InputList(MutableMapping):
             IndexError: if index > len(self)
 
         >>> pl = InputList([42])
-        >>> pl.mark(0, 'head')
+        >>> pl.mark(0, "head")
         >>> pl.head == 42
         True
-        '''
+        """
         if index >= len(self):
-            raise IndexError('list index out of range')
+            raise IndexError("list index out of range")
 
         reverse_indices = {i: k for k, i in self._indices.items()}
         if index in reverse_indices:
@@ -434,7 +434,7 @@ class InputList(MutableMapping):
         self._indices.clear()
 
     def create_group(self, name):
-        '''
+        """
         Add a new empty sublist under the given key.
 
         Args:
@@ -444,11 +444,11 @@ class InputList(MutableMapping):
             InputList: the newly created sublist
 
         >>> pl = InputList({})
-        >>> pl.create_group('group_name')
+        >>> pl.create_group("group_name")
         InputList([])
         >>> list(pl.group_name)
         []
-        '''
+        """
         self[name] = self.__class__()
         return self[name]
 
