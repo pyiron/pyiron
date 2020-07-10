0f4244db416b4b2db620098508343dc1ad68a372
Marvin Poul <ponder@creshal.de>
2020-07-10 10:20:45 +0200

    add test prints

--- .ci_support/travis_test_backwards.sh
+++ .ci_support/travis_test_backwards.sh
@@ -6,13 +6,17 @@
 # env
 
 pip install --force --ignore-installed --no-deps pyiron==0.2.16
+echo "Before save";
 for t in tests/backwards/*save.py; do
+    echo "Running $t";
     python $t
 done
 
 pip install --force --ignore-installed --no-deps .
 i=0;
+echo "Before loading";
 for t in tests/backwards/*load.py; do
+    echo "Running $t";
     python $t || i=$((i+1));
 done
 
