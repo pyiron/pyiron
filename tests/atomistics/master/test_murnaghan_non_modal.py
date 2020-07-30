e765f3341e4e0106cccbc99cc08d312da6e14364
Jan Janssen <jan-janssen@users.noreply.github.com>
2020-07-30 13:19:47 +0200

    Update test_murnaghan_non_modal.py

--- tests/atomistics/master/test_murnaghan_non_modal.py
+++ tests/atomistics/master/test_murnaghan_non_modal.py
@@ -65,7 +65,7 @@ class TestMurnaghan(unittest.TestCase):
         murn.server.run_mode.non_modal = True
         murn.run()
         self.assertFalse(job_ser.status.finished)
-        self.project.wait_for_job(murn, interval_in_s=5, max_iterations=50)
+        self.project.wait_for_job(murn, interval_in_s=5, max_iterations=60)
         self.assertTrue(murn.status.finished)
         murn.remove()
         job_ser.remove()
