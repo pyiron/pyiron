70efb82e631105f49fc445bf37474fca6c295743
Sudarsan Surendralal <surendralal@mpie.de>
2020-08-11 11:42:37 +0200

    Update ase_test.py
    test

--- .ci_support/ase_test.py
+++ .ci_support/ase_test.py
@@ -3,6 +3,10 @@ import pyiron.atomistics.structure.atoms as atoms
 import pyiron.atomistics.structure.atom as atom
 import pytest
 
+import ase
+
+print(ase.__file__)
+
 sys.modules["ase.atom"] = atom
 sys.modules["ase.atoms"] = atoms
 pytest.main(["ase/ase/test/cell"])
