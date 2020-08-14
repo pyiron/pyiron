f96912b389d87f8702ac91f8f423cb6ead58f358
Sudarsan Surendralal <surendralal@mpie.de>
2020-08-14 12:22:43 +0200

    Create travis_setup_ase_tests.sh

---
+++ .ci_support/travis_setup_ase_tests.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Install additional requirements 
+conda env update --name root --file .ci_support/environment_ase_tests.yml
+
+# Create .pyiron config
+printf "[DEFAULT]\nTOP_LEVEL_DIRS = ${HOME}\nRESOURCE_PATHS =$HOME/miniconda/share/pyiron" > ${HOME}/.pyiron
+
+# Downloading ase and overwriting PYTHONPATH
+git clone https://gitlab.com/ase/ase.git
+export PYTHONPATH="/home/travis/build/pyiron/pyiron/ase:$PYTHONPATH"
