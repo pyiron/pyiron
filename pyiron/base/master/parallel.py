abd2fa385283c2773f1f9b6fb49e89423d1d9be7
Jan Janßen <janssen@mpie.de>
2020-08-19 22:32:36 +0200

    Support for nested GenericMasters

--- pyiron/base/master/parallel.py
+++ pyiron/base/master/parallel.py
@@ -608,6 +608,11 @@ class ParallelMaster(GenericMaster):
         if self.is_finished():
             self.status.collect = True
             self.run()
+        elif self.status.busy:
+            self.status.refresh = True
+            self.run_if_refresh()
+        else:
+            self.status.suspended = True
 
     def _run_if_master_modal_child_non_modal(self, job):
         """
