fa02bb600ee540eef71aaf7ea8d6e847f43da43c
sudarsan1989 <surendralal@mpie.de>
2020-08-24 10:12:16 +0200

    Overwriting base function

--- pyiron/vasp/base.py
+++ pyiron/vasp/base.py
@@ -2274,6 +2274,14 @@ LORBIT = 0
 """
         self.load_string(file_content)
 
+    def _bool_str_to_bool(self, val):
+        val = super(Incar, self)._bool_str_to_bool(val)
+        extra_bool = {True: "T", False: "F"}
+        for key, value in extra_bool.items():
+            if val == value:
+                return key
+        return val
+
 
 class Kpoints(GenericParameters):
     """
