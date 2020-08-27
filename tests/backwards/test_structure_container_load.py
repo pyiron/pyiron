6c8c7322c94bc772288ef592b0aab596966ae052
Marvin Poul <ponder@creshal.de>
2020-08-27 20:19:45 +0200

    don't convert_to_object in load tests
    otherwise all backwards compatibility tests will fail, if even only one
    job class fails in from_hdf

--- tests/backwards/test_structure_container_load.py
+++ tests/backwards/test_structure_container_load.py
@@ -1,5 +1,6 @@
 from pyiron import Project, __version__
 pr = Project("tests/static/backwards")
-for job in pr.iter_jobs(recursive = True):
-    if job.name == "structurecontainer":
+for job in pr.iter_jobs(recursive = True, convert_to_object = False):
+    if job.name == "structure_container":
+        job = job.to_object()
         job.run()
--- tests/backwards/test_structure_container_save.py
+++ tests/backwards/test_structure_container_save.py
@@ -3,6 +3,6 @@ pr = Project(
     "tests/static/backwards/V{}".format(__version__).replace(".", "_")
 )
 structure = pr.create_ase_bulk("Al")
-job = pr.create_job(pr.job_type.StructureContainer, "structurecontainer")
+job = pr.create_job(pr.job_type.StructureContainer, "structure_container")
 job.structure = structure
 job.save()
