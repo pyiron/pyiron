1a31812627e2994a8db24e7a98fa29ce38afddc3
sudarsan1989 <surendralal@mpie.de>
2020-08-28 20:53:36 +0200

    :zap: Module for OSZICAR parsing

--- pyiron/vasp/oszicar.py
+++ pyiron/vasp/oszicar.py
@@ -2,6 +2,7 @@
 # Copyright (c) Max-Planck-Institut für Eisenforschung GmbH - Computational Materials Design (CM) Department
 # Distributed under the terms of "New BSD License", see the LICENSE file.
 
+import numpy as np
 
 __author__ = "Sudarsan Surendralal"
 __copyright__ = (
@@ -32,6 +33,14 @@ class Oszicar(object):
     def from_file(self, filename="OSZICAR"):
         with open(filename, "r") as f:
             lines = f.readlines()
+        self.parse_dict["energy_pot"] = self.get_energy_pot(lines)
 
-    def get_energy_pot(self):
-        pass
+    @staticmethod
+    def get_energy_pot(lines):
+        trigger = "F="
+        energy_list = list()
+        for i, line in enumerate(lines):
+            line = line.strip()
+            if trigger in line:
+                energy_list.append(float(lines[i-1].strip().split()[2]))
+        return np.array(energy_list)
