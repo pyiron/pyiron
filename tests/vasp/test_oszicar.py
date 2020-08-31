9d591b22e2f04f2b1ea6a12a896661f7048273fe
sudarsan1989 <surendralal@mpie.de>
2020-08-31 18:32:46 +0200

    Using `np.allclose` instead

--- tests/vasp/test_oszicar.py
+++ tests/vasp/test_oszicar.py
@@ -35,8 +35,9 @@ class TestOszicar(unittest.TestCase):
         for filename in self.file_list:
             self.oszicar_parser.from_file(filename=filename)
             if "1" in filename:
-                self.assertTrue(np.array_equal(self.oszicar_parser.parse_dict["energy_pot"], [-17.7379867884]))
+                self.assertTrue(np.allclose(self.oszicar_parser.parse_dict["energy_pot"],
+                                            [-17.7379867884], rtol=1.e-10))
             if "2" in filename:
-                self.assertTrue(np.array_equal(self.oszicar_parser.parse_dict["energy_pot"],
-                                               [-1166.23382927, -1166.07589814, -1165.76905678,
-                                                -1165.69531250, -1165.85096438]))
+                self.assertTrue(np.allclose(self.oszicar_parser.parse_dict["energy_pot"],
+                                            [-1166.23382927, -1166.07589814, -1165.76905678,
+                                             -1165.69531250, -1165.85096438], rtol=1.e-10))
