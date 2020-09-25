8c381c7f614a37d4eb534c36be5bb63e22b80665
sudarsan1989 <surendralal@mpie.de>
2020-09-25 11:57:36 +0200

    Safer comparison

--- tests/vasp/test_oszicar.py
+++ tests/vasp/test_oszicar.py
@@ -34,10 +34,10 @@ class TestOszicar(unittest.TestCase):
     def test_energy_pot(self):
         for filename in self.file_list:
             self.oszicar_parser.from_file(filename=filename)
-            if "OSZICAR_1" in filename:
+            if "OSZICAR_1" in posixpath.split(filename)[-1]:
                 energies = self.oszicar_parser.parse_dict["energy_pot"]
                 self.assertTrue(np.array_equal(energies, [-17.7379867884]))
-            if "OSZICAR_2" in filename:
+            if "OSZICAR_2" in posixpath.split(filename)[-1]:
                 energies = self.oszicar_parser.parse_dict["energy_pot"]
                 self.assertTrue(np.array_equal(energies, [-1166.23382927, -1166.07589814, -1165.76905678,
                                                           -1165.69531250, -1165.85096438]))
