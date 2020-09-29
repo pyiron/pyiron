e4319653bd151983a4d1cf855ad88b8c4454c465
Sam Waseda <o.waseda@mpie.de>
2020-09-29 18:38:48 +0200

    test file added

---
+++ tests/atomistics/structure/test_neighbors.py
@@ -0,0 +1,24 @@
+# coding: utf-8
+# Copyright (c) Max-Planck-Institut für Eisenforschung GmbH - Computational Materials Design (CM) Department
+# Distributed under the terms of "New BSD License", see the LICENSE file.
+
+import unittest
+import numpy as np
+import os
+import warnings
+from pyiron.atomistics.structure.atoms import Atoms, CrystalStructure
+
+
+class TestAtoms(unittest.TestCase):
+    @classmethod
+    def tearDownClass(cls):
+        pass
+
+    @classmethod
+    def setUpClass(cls):
+        pass
+
+
+
+if __name__ == "__main__":
+    unittest.main()
