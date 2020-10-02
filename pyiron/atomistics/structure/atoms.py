489eca9cf9b84bdd62e76eaec1d658ee0fcfc6de
Sam Waseda <o.waseda@mpie.de>
2020-10-02 17:43:31 +0200

    probably removed error

--- pyiron/atomistics/structure/atoms.py
+++ pyiron/atomistics/structure/atoms.py
@@ -1712,12 +1712,12 @@ class Atoms(ASEAtoms):
         neighbor_distance_vec = []
         neighbor_indices = []
         for atom_id, index, distances in zip(id_list, neighbors[1], neighbors[0]):
-            neighbor_distance.append(distances[1:])
-            neighbor_indices.append(indices[index[1:]]%len(self))
+            neighbor_distance.append(distances[1:][index[1:]<len(indices)])
+            neighbor_indices.append(indices[index[1:][index[1:]<len(indices)]]%len(self))
             if np.max(distances)>width and include_boundary:
                 warnings.warn('boundary_width_factor may have been too small - most likely not all neighbors properly assigned')
             if t_vec:
-                neighbor_distance_vec.append(extended_positions[index[1:]]-self.positions[atom_id])
+                neighbor_distance_vec.append(extended_positions[index[1:][index[1:]<len(indices)]]-self.positions[atom_id])
         neighbor_obj.distances = neighbor_distance
         neighbor_obj.vecs = neighbor_distance_vec
         neighbor_obj.indices = neighbor_indices
