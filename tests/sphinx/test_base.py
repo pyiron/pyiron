96e0dfcea15febf98e07912ee743e538e06350d4
Jan Janßen <janssen@mpie.de>
2020-10-03 14:33:31 +0200

    Test potential list

--- tests/sphinx/test_base.py
+++ tests/sphinx/test_base.py
@@ -108,6 +108,9 @@ class TestSphinx(unittest.TestCase):
         self.assertEqual(len(self.sphinx.id_pyi_to_spx), len(self.sphinx.structure))
         self.assertEqual(len(self.sphinx.id_spx_to_pyi), len(self.sphinx.structure))
 
+    def test_potential(self):
+        print("spx_pot_test", self.sphinx.list_potentials(), self.sphinx_2_3.list_potentials(), self.sphinx_2_5.list_potentials())
+
     def test_write_input(self):
 
         file_content = [
--- tests/vasp/test_vasp.py
+++ tests/vasp/test_vasp.py
@@ -56,6 +56,9 @@ class TestVasp(unittest.TestCase):
     def setUp(self):
         self.job.structure = None
 
+    def test_potential(self):
+        print("vasp_pot_test", self.job.list_potentials(), self.job_spin.list_potentials(), self.job_complete.list_potentials())
+
     def test_init(self):
         self.assertEqual(self.job.__name__, "Vasp")
         self.assertEqual(self.job._sorted_indices, None)
