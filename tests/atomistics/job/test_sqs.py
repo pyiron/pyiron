9ac2b756e7ea680ae72d45aae6b7a0d46880183f
Sam Waseda <o.waseda@mpie.de>
2020-10-07 09:27:51 +0200

    unit test file added

---
+++ tests/atomistics/job/test_sqs.py
@@ -0,0 +1,27 @@
+# coding: utf-8
+# Copyright (c) Max-Planck-Institut für Eisenforschung GmbH - Computational Materials Design (CM) Department
+# Distributed under the terms of "New BSD License", see the LICENSE file.
+
+import os
+import unittest
+from pyiron.project import Project
+
+class TestSQS(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.file_location = os.path.dirname(os.path.abspath(__file__))
+        cls.project = Project(os.path.join(cls.file_location, "test_sqs"))
+        cls.project.remove_jobs_silently(recursive=True)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.file_location = os.path.dirname(os.path.abspath(__file__))
+        cls.project.remove(enable=True, enforce=True)
+
+    def test_run(self):
+        self.project.create_job(
+            'SQSJob', "job_test"
+        )
+
+if __name__ == "__main__":
+    unittest.main()
