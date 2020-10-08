c3f3674ba131737b63ec9ab6e8c946799a5e5983
Sam Waseda <o.waseda@mpie.de>
2020-10-08 13:34:18 +0200

    add tests

--- tests/atomistics/job/test_sqs.py
+++ tests/atomistics/job/test_sqs.py
@@ -5,6 +5,7 @@
 import os
 import unittest
 from pyiron.project import Project
+from pyiron.atomistics.structure.atoms import CrystalStructure
 
 
 class TestSQS(unittest.TestCase):
@@ -23,9 +24,23 @@ class TestSQS(unittest.TestCase):
         try:  # Only test if the machine has access to sqsgenerator -- at time of writing Windows doesn't
             from sqsgenerator.core.sqs import ParallelSqsIterator
 
-            self.project.create_job(
+            job = self.project.create_job(
                 'SQSJob', "job_test"
             )
+            structure = CrystalStructure("Al", bravais_basis="fcc", lattice_constant=4)
+            job.structure = structure
+            with self.assertRaises(ValueError):
+                job.validate_ready_to_run()
+            job.input.mole_fractions = dict()
+            structure[0] = 'Sc'
+            job.structure = structure
+            job.validate_ready_to_run()
+            job.input.mole_fractions = dict()
+            mole_fractions = {'Al': 0.5, 'Sc': 0.5}
+            job.input.mole_fractions = mole_fractions.copy()
+            job.structure = structure
+            job.validate_ready_to_run()
+            self.assertAlmostEqual(job.input.mole_fractions['Al'], mole_fractions['Al'])
         except ImportError:
             pass
 
