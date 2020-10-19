8b298bd0acf2d27bebf2d83193bcc3243dc909f8
Sam Waseda <o.waseda@mpie.de>
2020-10-19 22:09:40 +0200

    add tests

--- tests/interactive/test_scipyminimizer.py
+++ tests/interactive/test_scipyminimizer.py
@@ -13,14 +13,6 @@ class TestSxExtOptInteractive(unittest.TestCase):
     def setUpClass(cls):
         cls.file_location = os.path.dirname(os.path.abspath(__file__))
         cls.project = Project(os.path.join(cls.file_location, "../static/minim"))
-        cls.basis = cls.project.create_structure("Fe", "bcc", 2.8)
-        job = cls.project.create_job( 'HessianJob', "job_single")
-        job.server.run_mode.interactive = True
-        job.set_reference_structure(cls.basis)
-        job.set_force_constants(1)
-        job.structure.positions[0,0] += 0.01
-        cls.minim = job.create_job("ScipyMinimizer", "job_scipy")
-        cls.minim.input.ionic_force_tolerance = 1e-8
 
     @classmethod
     def tearDownClass(cls):
@@ -28,8 +20,25 @@ class TestSxExtOptInteractive(unittest.TestCase):
         cls.project.remove(enable=True, enforce=True)
 
     def test_run(self):
-        self.minim.run()
-        self.assertAlmostEqual(np.linalg.norm(self.minim.ref_job['output/generic/forces'][-1], axis=-1).max(), 0)
+        basis = self.project.create_structure("Fe", "bcc", 2.8)
+        job = self.project.create_job( 'HessianJob', "job_single")
+        job.server.run_mode.interactive = True
+        job.set_reference_structure(basis)
+        job.set_force_constants(1)
+        job.structure.positions[0,0] += 0.01
+        minim = job.create_job("ScipyMinimizer", "job_scipy")
+        minim.input.ionic_force_tolerance = 1e-8
+        minim.run()
+        self.assertAlmostEqual(np.linalg.norm(minim.ref_job['output/generic/forces'][-1], axis=-1).max(), 0)
+
+    def test_calc_minimize(self):
+        minim = self.project.create_job('ScipyMinimizer', 'calc_minimize')
+        with self.assertRaises(ValueError):
+            minim.calc_minimize(volume_only=True, pressure=None)
+        minim.calc_minimize(pressure=0)
+        self.assertTrue(np.array_equal(minim.input.pressure, np.zeros((3,3))))
+        minim.calc_minimize(pressure=1)
+        self.assertTrue(np.array_equal(minim.input.pressure, np.eye(3)))
 
 if __name__ == "__main__":
     unittest.main()
