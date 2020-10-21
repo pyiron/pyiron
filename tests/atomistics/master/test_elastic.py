b0acca2e26207c773ccefc21c6ffe8a370ff70d6
Sam Waseda <o.waseda@mpie.de>
2020-10-21 21:45:25 +0200

    test file added

---
+++ tests/atomistics/master/test_elastic.py
@@ -0,0 +1,29 @@
+# coding: utf-8
+# Copyright (c) Max-Planck-Institut für Eisenforschung GmbH - Computational Materials Design (CM) Department
+# Distributed under the terms of "New BSD License", see the LICENSE file.
+
+import os
+import unittest
+from pyiron.atomistics.structure.atoms import CrystalStructure
+from pyiron_base import Project
+
+
+class TestMurnaghan(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.file_location = os.path.dirname(os.path.abspath(__file__))
+        cls.project = Project(os.path.join(cls.file_location, "test_elast"))
+        cls.basis = CrystalStructure(
+            element="Fe", bravais_basis="fcc", lattice_constant=3.5
+        )
+        cls.project.remove_jobs_silently(recursive=True)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.file_location = os.path.dirname(os.path.abspath(__file__))
+        cls.project.remove(enable=True, enforce=True)
+
+
+
+if __name__ == "__main__":
+    unittest.main()
