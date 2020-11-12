dc7a22756464bc2821214c22b48f813dd825896a
Jan Janssen <jan-janssen@users.noreply.github.com>
2020-11-12 09:28:29 +0100

    Update test_backwards.sh

--- .ci_support/test_backwards.sh
+++ .ci_support/test_backwards.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-pip install --force-reinstall --ignore-installed --no-deps pyiron==0.2.16
+pip install --ignore-installed --no-deps pyiron==0.2.16
 echo "Before save";
 for t in tests/backwards/*save.py; do
     echo "Running $t";
     python $t
 done
 
-pip install --force-reinstall --ignore-installed --no-deps .
+pip install --ignore-installed --no-deps .
 i=0;
 echo "Before loading";
 for t in tests/backwards/*load.py; do
