b8beab016d6b643a7e363c345edd5032da2f895f
Jan Janssen <jan-janssen@users.noreply.github.com>
2020-11-18 14:55:58 +0100

    Update test_pyscal.py

--- tests/atomistics/structure/test_pyscal.py
+++ tests/atomistics/structure/test_pyscal.py
@@ -94,7 +94,7 @@ class Testpyscalatoms(unittest.TestCase):
         self.assertTrue(all([np.isclose(v, 8.7025) for v in self.ti_hcp.analyse_pyscal_centro_symmetry()]))
         print(self.si_dia.analyse_pyscal_centro_symmetry())
         self.assertTrue(all([
-            np.isclose(v, 22.113675) or np.isclose(v, 16.58525625)
+            np.isclose(v, 22.113675) or np.isclose(v, 16.58525625) or np.isclose(v, 11.0568375)
             for v in self.si_dia.analyse_pyscal_centro_symmetry()
         ]))
         self.assertEqual(len(self.al_fcc.analyse_pyscal_centro_symmetry()), len(self.al_fcc))
