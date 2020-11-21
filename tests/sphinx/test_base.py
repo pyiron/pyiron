c9845a09886dc663b14fd4f4fcfd39f02fdba215
Sam Waseda <o.waseda@mpie.de>
2020-11-21 08:37:48 +0100

    correct test k-mesh test

--- tests/sphinx/test_base.py
+++ tests/sphinx/test_base.py
@@ -270,7 +270,7 @@ class TestSphinx(unittest.TestCase):
         self.assertTrue("kPoints" not in self.sphinx_band_structure.input.sphinx.basis)
         self.assertEqual(self.sphinx_band_structure.input.KpointFolding, mesh)
         self.assertEqual(self.sphinx_band_structure.input.KpointCoords, center_shift)
-        self.assertEqual(self.sphinx_band_structure.get_k_mesh_by_cell(2.81/2/np.pi), [1,1,1])
+        self.assertEqual(self.sphinx_band_structure.get_k_mesh_by_cell(2*np.pi/2.81), [1,1,1])
 
     def test_set_empty_states(self):
         with self.assertRaises(ValueError):
