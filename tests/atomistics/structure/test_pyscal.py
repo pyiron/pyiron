d1588b01a17bb4a1e1e9f928fb7ecf4ff35164e6
Jan Janßen <janssen@mpie.de>
2020-11-21 12:10:46 +0100

    debug bcc

--- tests/atomistics/structure/test_pyscal.py
+++ tests/atomistics/structure/test_pyscal.py
@@ -90,6 +90,7 @@ class Testpyscalatoms(unittest.TestCase):
 
     def test_analyse_pyscal_centro_symmetry(self):
         self.assertTrue(all([np.isclose(v, 0.0) for v in self.al_fcc.analyse_pyscal_centro_symmetry()]))
+        print("Fe bcc: ", self.fe_bcc.analyse_pyscal_centro_symmetry())
         self.assertTrue(all([np.isclose(v, 6.177675) for v in self.fe_bcc.analyse_pyscal_centro_symmetry()]))
         self.assertTrue(all([np.isclose(v, 8.7025) for v in self.ti_hcp.analyse_pyscal_centro_symmetry()]))
         self.assertTrue(all([
