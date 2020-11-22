3d3f49818466be15023068380342d0d5eb8aaacb
Sam Waseda <o.waseda@mpie.de>
2020-11-22 10:48:23 +0100

    add planes

--- pyiron/atomistics/structure/analyse.py
+++ pyiron/atomistics/structure/analyse.py
@@ -28,7 +28,7 @@ class Analyse:
         """
         self._structure = structure
 
-    def get_layers(self, distance_threshold=0.01, id_list=None, wrap_atoms=True):
+    def get_layers(self, distance_threshold=0.01, id_list=None, wrap_atoms=True, planes=None):
         """
         Get an array of layer numbers.
 
@@ -38,6 +38,11 @@ class Analyse:
                 description: sklearn.cluster.AgglomerativeClustering
             id_list (list/numpy.ndarray): List of atoms for which the layers
                 should be considered.
+            planes (list/numpy.ndarray): Planes along which the layers are calculated. Planes are
+                given in vectors, i.e. [1, 0, 0] gives the layers along the x-axis. Default planes
+                are orthogonal unit vectors: [[1, 0, 0], [0, 1, 0], [0, 0, 1]]. If you have a
+                tilted box and want to calculate the layers along the directions of the cell
+                vectors, use `planes=np.linalg.inv(structure.cell).T`.
 
         Returns: Array of layer numbers (same shape as structure.positions)
 
@@ -49,17 +54,21 @@ class Analyse:
         Example II - get layers of only one species:
 
         >>> print('Iron layers:', structure.analyse.get_layers(
-        ...     id_list=structure.select_index('Fe'))
-        ... )
+        ...       id_list=structure.select_index('Fe')))
         """
+        if distance_threshold <= 0:
+            raise ValueError('distance_threshold must be a positive float')
         if id_list is None:
             id_list = np.arange(len(self._structure))
         if len(id_list)==0:
             raise ValueError('id_list must contain at least one id')
-        layers = []
         positions = self._structure.positions[np.array(id_list)]
         if wrap_atoms:
             positions = self._structure.get_wrapped_coordinates(positions)
+        if planes is not None:
+            mat = np.asarray(planes).reshape(-1, 3)
+            positions = np.einsum('ij,i,nj->ni', mat, 1/np.linalg.norm(mat, axis=-1), positions)
+        layers = []
         for x in positions.T:
             cluster = AgglomerativeClustering(
                 linkage='complete',
@@ -81,4 +90,6 @@ class Analyse:
             # permutation array according to the value in the permuation array. i.e.
             # argsorting.
             layers.append(x[args].argsort().argsort()[cluster.labels_])
+        if planes is not None and len(np.asarray(planes).shape)==1:
+            return np.asarray(layers).flatten()
         return np.vstack(layers).T
