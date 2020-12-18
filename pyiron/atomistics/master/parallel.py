b78edfd927afcb2738fd35ec1a9509e7286a52ea
Jan Janßen <janssen@mpie.de>
2020-12-18 11:21:10 +0100

    Fix ParallelMaster
    Fix https://github.com/pyiron/pyiron/issues/1267

--- pyiron/atomistics/master/parallel.py
+++ pyiron/atomistics/master/parallel.py
@@ -27,14 +27,14 @@ class AtomisticParallelMaster(ParallelMaster, AtomisticGenericJob):
 
     @property
     def structure(self):
-        if self.ref_job:
+        if self.ref_job is not None:
             return self._ref_job.structure
         else:
             return None
 
     @structure.setter
     def structure(self, basis):
-        if self.ref_job:
+        if self.ref_job is not None:
             self._ref_job.structure = basis
         else:
             raise ValueError(
