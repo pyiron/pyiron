b832c9dfb565f1c6ed5e38f65d3e27466687085b
Niklas Siemer <70580458+niklassiemer@users.noreply.github.com>
2021-01-08 12:11:40 +0100

    Revert rename in setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 import versioneer
 
 setup(
-    name='pyiron_atomistic',
+    name='pyiron',
     version=versioneer.get_version(),
     description='pyiron - an integrated development environment (IDE) for computational materials science.',
     long_description='http://pyiron.org',
@@ -28,7 +28,7 @@ setup(
                  'Programming Language :: Python :: 3.8',
                  'Programming Language :: Python :: 3.9'],
 
-    keywords='pyiron_atomistic',
+    keywords='pyiron',
     packages=find_packages(exclude=["*tests*", "*docs*", "*binder*", "*conda*", "*notebooks*", "*.ci_support*"]),
     install_requires=[
         'ase>=3.20.1',
