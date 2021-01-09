1e7dd25c555e6998e79b35d85571670509c42e90
Jan Janssen <jan-janssen@users.noreply.github.com>
2021-01-09 09:58:04 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 import versioneer
 
 setup(
-    name='pyiron',
+    name='pyiron_atomistic',
     version=versioneer.get_version(),
     description='pyiron - an integrated development environment (IDE) for computational materials science.',
     long_description='http://pyiron.org',
