8dfafd212bebb434378e1ff4ce7a61a2b8bb54ad
Jan Janßen <janssen@mpie.de>
2021-01-10 10:05:14 +0100

    Remove unit tests

--- tests/backwards/test_structure_container_load.py
+++
@@ -1,6 +0,0 @@
-from pyiron import Project, __version__
-pr = Project("tests/static/backwards")
-for job in pr.iter_jobs(recursive = True, convert_to_object = False):
-    if job.name == "structure_container":
-        job = job.to_object()
-        job.run()
