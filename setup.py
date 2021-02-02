a2a7b5814911351529da515a8318037095b3ba65
Jan Janssen <jan-janssen@users.noreply.github.com>
2021-02-02 19:26:27 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
     keywords='pyiron',
     packages=find_packages(exclude=["*tests*", "*docs*", "*binder*", "*conda*", "*notebooks*", "*.ci_support*"]),
     install_requires=[
-        'pyiron_atomistics==0.2.2'
+        'pyiron_atomistics==0.2.3'
     ],
     cmdclass=versioneer.get_cmdclass(),
 
