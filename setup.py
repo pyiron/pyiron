bfe19c29f083183925c6e4211f4395c0a92ea4b9
Liam Huber <liamhuber@greyhavensolutions.com>
2023-07-05 09:24:34 -0700

    Remove pypi support for python versions not allowed by conda (#1500)

--- setup.py
+++ setup.py
@@ -20,9 +20,6 @@ setup(
                  'License :: OSI Approved :: BSD License',
                  'Intended Audience :: Science/Research',
                  'Operating System :: OS Independent',
-                 'Programming Language :: Python :: 3.6',
-                 'Programming Language :: Python :: 3.7',
-                 'Programming Language :: Python :: 3.8',
                  'Programming Language :: Python :: 3.9',
                  'Programming Language :: Python :: 3.10'],
 
